1e87021b5066754fcaa436517297aa6227967769
Ian <aristophian@googlemail.com>
2016-09-03 12:37:59 +0200

    [autoupdater] - Allow beta updates without throwing an Exception in checkIntegrity() method

--- serendipity_event_autoupdate/serendipity_event_autoupdate.php
+++ serendipity_event_autoupdate/serendipity_event_autoupdate.php
@@ -18,7 +18,7 @@ class serendipity_event_autoupdate extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_AUTOUPDATE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'onli, Ian');
-        $propbag->add('version',       '1.2.1');
+        $propbag->add('version',       '1.2.2');
         $propbag->add('configuration', array('download_url', 'releasefile_url'));
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
@@ -618,6 +618,9 @@ EOS;
         $updateDir    = (string)$serendipity['serendipityPath'] . 'templates_c/' . "serendipity-$version/";
         $checksumFile = (string)$updateDir . "serendipity/checksums.inc.php";
 
+        if (strpos($version, 'beta') !== FALSE) {
+            return true;
+        }
         include_once $checksumFile;
 
         $checksums = $serendipity['checksums_' . $version];
