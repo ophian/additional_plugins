57732963370c85b9ea5ed64f2ef859d1b1ca2ff1
Ian <aristophian@googlemail.com>
2016-10-27 14:43:06 +0200

    [linktrimmer] - Sync iconfont accessibility fixes

--- serendipity_event_linktrimmer/serendipity_event_linktrimmer.php
+++ serendipity_event_linktrimmer/serendipity_event_linktrimmer.php
@@ -22,7 +22,7 @@ class serendipity_event_linktrimmer extends serendipity_event
             'php'         => '5.2.0'
         ));
 
-        $propbag->add('version',       '1.6.6');
+        $propbag->add('version',       '1.6.7');
         $propbag->add('author',        'Garvin Hicking, Ian');
         $propbag->add('stackable',     false);
         $propbag->add('configuration', array('prefix', 'frontpage', 'domain'));
