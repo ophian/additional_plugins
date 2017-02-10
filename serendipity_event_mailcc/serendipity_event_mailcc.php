d90a8a52c3df2645c89cea6efd7232caf0fbfd56
Ian <aristophian@googlemail.com>
2017-02-10 15:26:45 +0100

    Remove serendipity_event_mailcc from alpha

--- serendipity_event_mailcc/serendipity_event_mailcc.php
+++ serendipity_event_mailcc/serendipity_event_mailcc.php
@@ -1,33 +1,42 @@
-<?php # 
+<?php
 
 if (IN_serendipity !== true) {
     die ("Don't hack!");
 }
 
+// Load possible language files.
+#@serendipity_plugin_api::load_language(dirname(__FILE__));
+
 class serendipity_event_mailcc extends serendipity_event
 {
     var $title = 'Adds CC to all sent emails';
 
-    function introspect(&$propbag) {
+    function introspect(&$propbag)
+    {
         global $serendipity;
 
         $propbag->add('name',          'Adds CC to all sent emails');
-        $propbag->add('description',   '(Notice: Make sure that the all of your authors have the option to receiv comment notification emails activated, or else no mails will be created that can be CCed');
+        $propbag->add('description',   '(Notice: Make sure that the all of your authors have the option to receive comment notification emails activated, or else no mails will be created that can be CCed');
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Garvin Hicking');
-        $propbag->add('version',       '1.01');
-        $propbag->add('requirements',  array('serendipity' => '0.8','smarty'      => '2.6.7','php'         => '4.1.0'
+        $propbag->add('version',       '1.02');
+        $propbag->add('requirements',  array(
+            'serendipity' => '0.8',
+            'smarty'      => '2.6.7',
+            'php'         => '4.1.0'
         ));
         $propbag->add('groups', array('BACKEND_FEATURES'));
         $propbag->add('event_hooks',   array('backend_sendmail' => true));
         $propbag->add('configuration', array('cc'));
     }
 
-    function generate_content(&$title) {
+    function generate_content(&$title)
+    {
         $title = $this->title;
     }
 
-    function introspect_config_item($name, &$propbag) {
+    function introspect_config_item($name, &$propbag)
+    {
         switch($name) {
             case 'cc':
             $propbag->add('type',        'string');
@@ -39,7 +48,8 @@ class serendipity_event_mailcc extends serendipity_event
         return true;
     }
 
-    function event_hook($event, &$bag, &$eventData, $addData = null) {
+    function event_hook($event, &$bag, &$eventData, $addData = null)
+    {
         global $serendipity;
         $hooks = &$bag->get('event_hooks');
 
@@ -47,15 +57,18 @@ class serendipity_event_mailcc extends serendipity_event
             switch($event) {
                 case 'backend_sendmail':
                     $eventData['headers'][] = 'CC: ' . $this->get_config('cc');
-                    return true;
                     break;
 
-              default:
-                return false;
+                default:
+                    return false;
+
             }
+            return true;
         } else {
             return false;
         }
     }
 
 }
+
+?>
