cf3e80e2927a3143713becacc63f72f2d3ca5b35
Ian <aristophian@googlemail.com>
2017-03-22 17:27:35 +0100

    imagesidebar: Use modern constructor method

--- serendipity_plugin_imagesidebar/serendipity_plugin_imagesidebar.php
+++ serendipity_plugin_imagesidebar/serendipity_plugin_imagesidebar.php
@@ -13,7 +13,7 @@ class subplug_sidebar
 {
     var $changed = false;
 
-    function subplug_sidebar($setting_array = NULL)
+    function __construct($setting_array = NULL)
     {
        if (is_array($setting_array)) {
            foreach ($setting_array AS $name => $value) {
@@ -74,7 +74,7 @@ class serendipity_plugin_imagesidebar extends serendipity_plugin
         $propbag->add('description',   PLUGIN_SIDEBAR_IMAGESIDEBAR_DESC . ' PLEASE NOTE: This plugin has been checked working with recent Serendipity installs for the Serendipity Media Library only. You will find limitations and old legacy code with at least the Menalto Gallery (dead project) and modern PHP using old MySQL extension code API calls.');
         $propbag->add('stackable',     true);
         $propbag->add('author',        'Andrew Brown (Menalto code), Matthew Groeninger (Unified/Media Lib. Code), Stefan Lange-Hegermann (Zooomr Code), Matthew Maude (Coppermine code), Ian');
-        $propbag->add('version',       '1.10');
+        $propbag->add('version',       '1.11');
         $propbag->add('license',       'BSD');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
