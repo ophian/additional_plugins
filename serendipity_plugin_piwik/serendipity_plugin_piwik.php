cdde2aa751bb6ba95f7294503109553ceb0c68e4
Ian <aristophian@googlemail.com>
2017-03-28 19:31:29 +0200

    plugin_piwik: Fix version of d4c61af and changelog

--- serendipity_plugin_piwik/serendipity_plugin_piwik.php
+++ serendipity_plugin_piwik/serendipity_plugin_piwik.php
@@ -36,8 +36,8 @@ class serendipity_plugin_piwik extends serendipity_plugin
         $propbag->add('name',          PLUGIN_SIDEBAR_PIWIK_NAME);
         $propbag->add('description',   PLUGIN_SIDEBAR_PIWIK_DESC);
         $propbag->add('stackable',     true);
-        $propbag->add('author',        'Bernd Distler');
-        $propbag->add('version',       '0.6');
+        $propbag->add('author',        'Bernd Distler, Ian');
+        $propbag->add('version',       '0.5.1');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
             'smarty'      => '2.6.7',
