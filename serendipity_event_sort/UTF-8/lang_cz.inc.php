5bc47182ceb215ee83869cbb7a0711ce402c3d33
Ian <aristophian@googlemail.com>
2017-04-25 14:10:47 +0200

    Remove deprecated plugin: event_sort

--- serendipity_event_sort/UTF-8/lang_cz.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- *  @author Vladimír Ajgl <vlada@ajgl.cz>
- *  @translated 2011/06/19
- */
-@define('PLUGIN_EVENT_SORT_TITLE', 'UTF-8 řazení');
-@define('PLUGIN_EVENT_SORT_DESC', 'Plugin nabízí speciální abecední třídění podle UTF-8 jako rozšíření pro specifické pluginy typu Freetag. Umožňuje řadit podle libovolné abecedy s jinými než anglickými písmeny, např. v azbuce, čínštině a jiných. Pomoc s vytvářením dalších abeced je vítaná (viz fórum serendipity).');
--- serendipity_event_sort/lang_cs.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- *  @author Vladimír Ajgl <vlada@ajgl.cz>
- *  @translated 2011/06/19
- */
-@define('PLUGIN_EVENT_SORT_TITLE', 'UTF-8 øazení');
-@define('PLUGIN_EVENT_SORT_DESC', 'Plugin nabízí speciální abecední tøídìní podle UTF-8 jako rozšíøení pro specifické pluginy typu Freetag. Umožòuje øadit podle libovolné abecedy s jinými než anglickými písmeny, napø. v azbuce, èínštinì a jiných. Pomoc s vytváøením dalších abeced je vítaná (viz fórum serendipity).');
