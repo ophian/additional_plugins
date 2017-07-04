f20fd3ff635fb82c06297f2658306edd001e78bf
Ian <aristophian@googlemail.com>
2017-07-04 15:07:13 +0200

    ckeplus: Upgrade to 4.7.1.0

--- serendipity_event_ckeditor/serendipity_event_ckeditor.php
+++ serendipity_event_ckeditor/serendipity_event_ckeditor.php
@@ -49,7 +49,7 @@ class serendipity_event_ckeditor extends serendipity_event
      * @access protected
      * @var string
      */
-    protected $cke_zipfile = 'ckeditor_4.7.0.0-plus.zip';
+    protected $cke_zipfile = 'ckeditor_4.7.1.0-plus.zip';
 
     /**
      * Access property checkUpdateVersion
@@ -57,16 +57,16 @@ class serendipity_event_ckeditor extends serendipity_event
      * Verify release package versions - do update on upgrades!
      * @var array
      */
-    protected $checkUpdateVersion = array('ckeditor:4.7.0.0');
+    protected $checkUpdateVersion = array('ckeditor:4.7.1.0');
 
     /**
      * Access property revisionPackage
      * Note revisions of ckeditor and plugin additions to lang files
      * @var array
      */
-    protected $revisionPackage = array('CKEditor 4.7.0 (revision 320013d, full package, 2017-06-08)',
+    protected $revisionPackage = array('CKEditor 4.7.1 (revision 08ffd39, full package, 2017-06-28)',
                                        'CKEditor-Plugin: mediaembed, v. 0.6+ (https://github.com/frozeman/MediaEmbed, 2016-07-24)',
-                                       'CKEditor-Plugin: manually added codesnippet, fakeobjects, lineutils, autogrow, widget and widgetselection plugins, 2017-06-08)',
+                                       'CKEditor-Plugin: manually added codesnippet, fakeobjects, lineutils, autogrow, widget and widgetselection plugins, 2017-06-28)',
                                        'CKEditor-Plugin: procurator, v. 1.6 (Serendipity placeholder Plugin, 2016-01-01)',
                                        'CKEditor-Plugin: cheatsheet, v. 1.2 (Serendipity CKE-Cheatsheet Plugin, 2016-08-15)',
                                        'CKEditor-S9yCustomConfig, cke_config.js, v. 2.11, 2017-04-29',
@@ -184,7 +184,7 @@ class serendipity_event_ckeditor extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_CKEDITOR_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Rustam Abdullaev, Ian');
-        $propbag->add('version',       '4.7.0.0'); // is CKEDITOR Series 4.7.0 - and appended plugin revision .0
+        $propbag->add('version',       '4.7.1.0'); // is CKEDITOR Series 4.7.1 - and appended plugin revision .0
         $propbag->add('copyright',     'GPL or LGPL License');
         $propbag->add('requirements',  array(
             'serendipity' => '1.7',
@@ -385,7 +385,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function updateConfig()
     {
-        #$this->temporaryDowngrade('4.7.0.0', '4.6.2.0'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.7.1.0', '4.7.0.0'); // was temporary used for the harmonization of plugin and lib versions
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
             $this->set_config('last_'.$match[0].'_version', $match[1]);
@@ -399,7 +399,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function checkUpdate()
     {
-        #$this->temporaryDowngrade('4.7.0.0', '4.6.2.0'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.7.1.0', '4.7.0.0'); // was temporary used for the harmonization of plugin and lib versions
         $doupdate = false;
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
