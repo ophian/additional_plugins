4404e320e4a21837bc6420e901386adbe141924a
Ian <aristophian@googlemail.com>
2017-11-03 12:01:07 +0100

    cc: Move core plugin creativecommons to Spartacus

---
+++ serendipity_plugin_creativecommons/UTF-8/lang_bg.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ *  @version
+ *  @author Ivan Cenov jwalker@hotmail.bg
+ */
+
+@define('PLUGIN_SIDEBAR_CREATIVECOMMONS_NAME', 'Creative Commons');
+@define('PLUGIN_SIDEBAR_CREATIVECOMMONS_DESC', 'Показва известяване от creative commons в страничния панел.');
---
+++ serendipity_plugin_creativecommons/lang_de.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+@define('PLUGIN_SIDEBAR_CREATIVECOMMONS_NAME', 'Creative Commons');
+@define('PLUGIN_SIDEBAR_CREATIVECOMMONS_DESC', 'Zeigt einen Creative Commons Hinweis an');
