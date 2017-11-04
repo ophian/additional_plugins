fb624023b35ff503304c9d98bba3d3c795eb2864
Ian <aristophian@googlemail.com>
2017-11-04 13:25:06 +0100

    templatedropdown: Move core sidebar plugin to Spartacus

---
+++ serendipity_plugin_templatedropdown/UTF-8/lang_cz.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *  @version: 1381
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  EN-Revision: Revision of lang_en.inc.php
+ */
+
+@define('PLUGIN_TEMPLATEDROPDOWN_NAME',         'Výběr šablony');
+@define('PLUGIN_TEMPLATEDROPDOWN_DESC',         'Zobrazuje rozbalovací políčko pro výběr šablony');
+@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT',       'Potvrzovací tlačítko?');
+@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT_DESC',  'Zobrazit potvrzovací tlačítko?');
+
---
+++ serendipity_plugin_templatedropdown/lang_en.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *  @version
+ *  @author Translator Name <yourmail@example.com>
+ *  EN-Revision: Revision of lang_en.inc.php
+ */
+
+@define('PLUGIN_TEMPLATEDROPDOWN_NAME', 'Theme dropdown');
+@define('PLUGIN_TEMPLATEDROPDOWN_DESC', 'Show a box to change themes');
+@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT', 'Submit button?');
+@define('PLUGIN_TEMPLATEDROPDOWN_SUBMIT_DESC', 'Show a submit button?');
+
