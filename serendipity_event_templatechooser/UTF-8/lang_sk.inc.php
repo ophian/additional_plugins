3c2cf579397ea608b02e207314e9cacb6858fd82
Ian <aristophian@googlemail.com>
2017-11-04 13:24:19 +0100

    templatechooser: Move core event plugin to Spartacus

---
+++ serendipity_event_templatechooser/UTF-8/lang_sk.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ *  @author Martin Matuška <martin@matuska.org>
+ *  EN-Revision: Revision of lang_en.inc.php
+ */
+
+@define('PLUGIN_EVENT_TEMPLATECHOOSER_NAME',     'Výber šablón');
+@define('PLUGIN_EVENT_TEMPLATECHOOSER_DESC',     'Umožnuje návštevníkom zmeniť šablónu (= vzhľad) podľa vlastného výberu');
+
