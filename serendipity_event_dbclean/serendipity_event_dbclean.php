54d938a52997c3c416f01f23f37bcfd8d56946eb
Ian <aristophian@googlemail.com>
2017-11-12 14:50:37 +0100

    dbclean: Major up

--- serendipity_event_dbclean/serendipity_event_dbclean.php
+++ serendipity_event_dbclean/serendipity_event_dbclean.php
@@ -17,13 +17,14 @@ class serendipity_event_dbclean extends serendipity_event
         $propbag->add('name',          PLUGIN_EVENT_DBCLEAN_NAME);
         $propbag->add('description',   PLUGIN_EVENT_DBCLEAN_DESC);
         $propbag->add('stackable',     false);
-        $propbag->add('author',        'Malte Paskuda, Matthias Mees');
-        $propbag->add('version',       '0.2.9');
+        $propbag->add('author',        'Malte Paskuda, Matthias Mees, Ian');
+        $propbag->add('version',       '0.3.0');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6'
         ));
         $propbag->add('event_hooks',   array(
                                     'backend_sidebar_admin'  => true,
+                                    'backend_sidebar_admin_appearance'  => true,
                                     'backend_sidebar_entries_event_display_dbclean'  => true,
                                     'external_plugin' => true,
                                     'css_backend' => true,
@@ -80,10 +81,11 @@ class serendipity_event_dbclean extends serendipity_event
 
                         $days = (int)$this->get_config('days');
                         if ($days > 0) {
-                            $this->cleanDB('cronjoblog', $days);
+                            if (class_exists('serendipity_event_cronjob')) $this->cleanDB('cronjoblog', $days);
+                            if (class_exists('serendipity_event_karma')) $this->cleanDB('karmalog', $days);
                             $this->cleanDB('spamblocklog', $days);
                             $this->cleanDB('spamblock_htaccess', $days);
-                            $this->cleanDB('visitors', $days);
+                            if (class_exists('serendipity_event_statistics')) $this->cleanDB('visitors', $days);
                             $this->cleanDB('referrers', $days);
                             $this->cleanDB('exits', $days);
                         }
@@ -96,24 +98,27 @@ class serendipity_event_dbclean extends serendipity_event
                             if (! (serendipity_checkPermission('siteConfiguration') || serendipity_checkPermission('blogConfiguration'))) {
                                 return;
                             }
-                            $days = $_REQUEST['days'];
+                            $days = $_POST['days'];
                             if (is_numeric($days)) {
-                                if (isset($_REQUEST['cronjoblog'])) {
+                                if (isset($_POST['cronjoblog']) && class_exists('serendipity_event_cronjob')) {
                                     $this->cleanDB('cronjoblog', $days);
                                 }
-                                if (isset($_REQUEST['spamblocklog'])) {
+                                if (isset($_POST['karmalog']) && class_exists('serendipity_event_karma')) {
+                                    $this->cleanDB('karmalog', $days);
+                                }
+                                if (isset($_POST['spamblocklog'])) {
                                     $this->cleanDB('spamblocklog', $days);
                                 }
-                                if (isset($_REQUEST['spamblock_htaccess'])) {
+                                if (isset($_POST['spamblock_htaccess'])) {
                                     $this->cleanDB('spamblock_htaccess', $days);
                                 }
-                                if (isset($_REQUEST['visitors'])) {
+                                if (isset($_POST['visitors']) && class_exists('serendipity_event_statistics')) {
                                     $this->cleanDB('visitors', $days);
                                 }
-                                if (isset($_REQUEST['referrers'])) {
+                                if (isset($_POST['referrers'])) {
                                     $this->cleanDB('referrers', $days);
                                 }
-                                if (isset($_REQUEST['exits'])) {
+                                if (isset($_POST['exits'])) {
                                     $this->cleanDB('exits', $days);
                                 }
                             }
@@ -124,14 +129,15 @@ class serendipity_event_dbclean extends serendipity_event
                         }
                         break;
 
+                case 'backend_sidebar_admin_appearance':
+                    echo '<li><a href="?serendipity[adminModule]=event_display&amp;serendipity[adminAction]=dbclean">' .PLUGIN_EVENT_DBCLEAN_NAME .'</a></li>';
+                    break;
+
                 case 'backend_sidebar_admin':
                     if ($serendipity['version'][0] < 2) {
                         echo '<li class="serendipitySideBarMenuLink serendipitySideBarMenuEntryLinks"><a href="?serendipity[adminModule]=event_display&amp;serendipity[adminAction]=dbclean">' .PLUGIN_EVENT_DBCLEAN_NAME .'</a></li>';
-                    } else {
-                        echo '<li><a href="?serendipity[adminModule]=event_display&amp;serendipity[adminAction]=dbclean">' .PLUGIN_EVENT_DBCLEAN_NAME .'</a></li>';
                     }
                     break;
-
 
                 case 'backend_sidebar_entries_event_display_dbclean':
                     $this->displayMenu();
@@ -179,29 +185,63 @@ class serendipity_event_dbclean extends serendipity_event
         }
     }
 
-    function cleanDB($table, $days)
+    private function cleanDB($table, $days)
     {
         global $serendipity;
         set_time_limit(0);
-        if($table == 'visitors') {
-            $sql = "DELETE
-                      FROM {$serendipity['dbPrefix']}visitors
-                     WHERE unix_timestamp(concat(day,' ',time)) <" . (time() - ($days*24*60*60));
+
+        if (in_array($table, array('visitors', 'referrers', 'exits'))) {
+            if (stristr($serendipity['dbType'], 'sqlite')) {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}$table
+                         WHERE strftime('%s', day) < (strftime('%s', 'now') - ($days*86400))";
+            }
+            else if (stristr($serendipity['dbType'], 'postgres')) {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}$table
+                         WHERE to_timestamp(day) < (NOW() - INTERVAL '$days days')";
+            }
+            else {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}$table
+                         WHERE UNIX_TIMESTAMP(day) < (UNIX_TIMESTAMP(NOW()) - ($days*86400))";
+            }
             serendipity_db_query($sql);
-        } else if ($table =='referrers') {
-             $sql = "DELETE
-                       FROM {$serendipity['dbPrefix']}$table
-                      WHERE day <" . (time() - ($days*24*60*60));
+        }
+        else if ($table == 'karmalog') {
+            if (stristr($serendipity['dbType'], 'sqlite')) {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}karmalog
+                         WHERE votetime < (strftime('%s', 'now') - ($days*86400))";
+            }
+            else if (stristr($serendipity['dbType'], 'postgres')) {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}karmalog
+                         WHERE votetime < (NOW() - INTERVAL '$days days')";
+            }
+            else {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}karmalog
+                         WHERE votetime < (UNIX_TIMESTAMP(NOW()) - ($days*86400))";
+            }
             serendipity_db_query($sql);
-        } else if ($table == 'exits') {
-             $sql = "DELETE
-                       FROM {$serendipity['dbPrefix']}$table
-                      WHERE day < '" . date( 'Y-m-d', (time() - ($days*24*60*60))) ."'";
-            serendipity_db_query($sql);
-        } else {
-            $sql = "DELETE
-                      FROM {$serendipity['dbPrefix']}$table
-                     WHERE timestamp < " . (time() - ($days*24*60*60));
+        }
+        if (in_array($table, array('cronjoblog', 'spamblocklog', 'spamblock_htaccess'))) {
+            if (stristr($serendipity['dbType'], 'sqlite')) {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}$table
+                         WHERE timestamp < (strftime('%s', 'now') - ($days*86400))";
+            }
+            else if (stristr($serendipity['dbType'], 'postgres')) {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}$table
+                         WHERE timestamp < (NOW() - INTERVAL '$days days')";
+            }
+            else {
+                $sql = "DELETE
+                          FROM {$serendipity['dbPrefix']}$table
+                         WHERE timestamp < (UNIX_TIMESTAMP(NOW()) - ($days*86400))";
+            }
             serendipity_db_query($sql);
         }
 
@@ -232,115 +272,108 @@ class serendipity_event_dbclean extends serendipity_event
     function displayMenu()
     {
         global $serendipity;
+
         if ($serendipity['version'][0] < 2) {
-            echo '<style>#dbcleanTable { width: 100%; text-align: center; } #dbcleanTable th { text-align: center; }</style>';
+            echo '<style>#dbcleanTable { width: 100%; text-align: center; } #dbcleanTable th { text-align: center; }</style>'."\n";
         }
 
-        echo '<h2>' . PLUGIN_EVENT_DBCLEAN_NAME_MENU . '</h2>';
+        echo '<h2>' . PLUGIN_EVENT_DBCLEAN_NAME_MENU . "</h2>\n";
 
-        echo '<form action="'.$serendipity ['baseURL'] . 'index.php?/plugin/dbclean' .'" method="post">';
+        echo '<form action="'.$serendipity ['baseURL'] . 'index.php?/plugin/dbclean' .'" method="post">'."\n";
         if ($serendipity['version'][0] < 2) {
-            echo PLUGIN_EVENT_DBCLEAN_MENU_KEEP . ' <input type="text" name="days" value="' . $this->get_config('days') . '" size="3" maxlength="3" /> ' . DAYS;
+            echo PLUGIN_EVENT_DBCLEAN_MENU_KEEP . ' <input type="text" name="days" value="' . $this->get_config('days') . '" size="3" maxlength="3" /> ' . DAYS ."\n";
         } else {
-            echo '<div class="form_field">';
-            echo '<label for="dbcleanup_days">' . PLUGIN_EVENT_DBCLEAN_MENU_KEEP . ' (' . DAYS . ')' . '</label>';
-            echo ' <input id="dbcleanup_days" type="text" name="days" value="' . $this->get_config('days') . '" size="3" maxlength="3">';
-            echo '</div>';
+            echo '<div class="form_field">'."\n";
+            echo '  <label for="dbcleanup_days">' . PLUGIN_EVENT_DBCLEAN_MENU_KEEP . ' (' . DAYS . ')' . "</label>\n";
+            echo '  <input id="dbcleanup_days" type="text" name="days" value="' . $this->get_config('days') . '" size="3" maxlength="3">'."\n";
+            echo "</div>\n";
         }
-
-        echo '<table id="dbcleanTable">';
-        echo '<thead>';
-        echo '<tr>';
-        echo '<th>' . DELETE . '</th>';
-        echo '<th>' . PLUGIN_EVENT_DBCLEAN_TABLE . '</th>';
-        echo '<th>' . ENTRIES . '</th>';
-        echo '</tr>';
-        echo '</thead>';
-        echo '<tbody>';
-        echo '<tr>';
-        echo '<td><input class="input_checkbox" type="checkbox" name="spamblocklog"  value="spamblocklog"  checked="checked" tabindex="1" /></td>';
-        echo '<td>spamblocklog</td>';
-        echo '<td>' . $this->countElements('spamblocklog') . '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td><input class="input_checkbox" type="checkbox" name="spamblock_htaccess"  value="spamblock_htaccess"  checked="checked" tabindex="1" /></td>';
-        echo '<td>spamblock_htaccess</td>';
-        echo '<td>' . $this->countElements('spamblock_htaccess') . '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td><input class="input_checkbox" type="checkbox" name="visitors"  value="visitors"  checked="checked" tabindex="1" /></td>';
-        echo '<td>visitors</td>';
-        echo '<td>' . $this->countElements('visitors') . '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td><input class="input_checkbox" type="checkbox" name="referrers"  value="referrers"  checked="checked" tabindex="1" /></td>';
-        echo '<td>referrers</td>';
-        echo '<td>' . $this->countElements('referrers') . '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td><input class="input_checkbox" type="checkbox" name="exits"  value="exits"  checked="checked" tabindex="1" /></td>';
-        echo '<td>exits</td>';
-        echo '<td>' . $this->countElements('exits') . '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td><input class="input_checkbox" type="checkbox" name="cronjoblog"  value="cronjoblog"  checked="checked" tabindex="1" /></td>';
-        echo '<td>cronjoblog</td>';
-        echo '<td>' . $this->countElements('cronjoblog') . '</td>';
-        echo '</tr>';
-        echo '</table>';
+        $thc = array(DELETE, PLUGIN_EVENT_DBCLEAN_TABLE, ENTRIES);
+        echo <<<EOS
+        <table id="dbcleanTable">
+          <thead>
+            <tr>
+              <th>{$thc[0]}</th>
+              <th>{$thc[1]}</th>
+              <th>{$thc[2]}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="spamblocklog" value="spamblocklog" checked="checked" tabindex="1" /></td>
+              <td>spamblocklog</td>
+              <td>{$this->countElements('spamblocklog')}</td>
+            </tr>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="spamblock_htaccess" value="spamblock_htaccess" checked="checked" tabindex="1" /></td>
+              <td>spamblock_htaccess</td>
+              <td>{$this->countElements('spamblock_htaccess')}</td>
+            </tr>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="visitors" value="visitors" checked="checked" tabindex="1" /></td>
+              <td>visitors</td>
+              <td>{$this->countElements('visitors')}</td>
+            </tr>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="referrers" value="referrers" checked="checked" tabindex="1" /></td>
+              <td>referrers</td>
+              <td>{$this->countElements('referrers')}</td>
+            </tr>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="exits" value="exits" checked="checked" tabindex="1" /></td>
+              <td>exits</td>
+              <td>{$this->countElements('exits')}</td>
+            </tr>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="cronjoblog" value="cronjoblog" checked="checked" tabindex="1" /></td>
+              <td>cronjoblog</td>
+              <td>{$this->countElements('cronjoblog')}</td>
+            </tr>
+            <tr>
+              <td><input class="input_checkbox" type="checkbox" name="karmalog" value="karmalog" checked="checked" tabindex="1" /></td>
+              <td>karmalog</td>
+              <td>{$this->countElements('karmalog')}</td>
+            </tr>
+        </table>
+EOS;
         if ($serendipity['version'][0] < 2) {
-            echo '<input type="submit" value="' . GO . '" tabindex="2" />';
+            echo '<input type="submit" value="' . GO . '" tabindex="2" />'."\n";
         } else {
-            echo '<div class="form_buttons"><input class="state_cancel" type="submit" value="' . DELETE . '"></div>';
+            echo '<div class="form_buttons"><input class="state_cancel" type="submit" value="' . DELETE . '"></div>'."\n";
         }
-        echo '</form>';
+    echo "</form>\n";
     }
 
-    function countElements($table, $timespan=false)
+    function countElements($table)
     {
         global $serendipity;
-        if (! $timespan) {
-            $sql = "SELECT COUNT(*)
-                    FROM {$serendipity['dbPrefix']}$table";
-        } else {
-            if ($table=='visitors') {
-                $sql = "SELECT COUNT(*)
-                    FROM {$serendipity['dbPrefix']}visitors
-                    WHERE unix_timestamp(concat(day,' ',time)) < $timespan";
-            } else if ($table =='referrers') {
-                $sql = "SELECT COUNT(*)
-                        FROM {$serendipity['dbPrefix']}$table
-                        WHERE day < $timespan";
-            } else {
-                $sql = "SELECT COUNT(*)
-                        FROM {$serendipity['dbPrefix']}$table
-                        WHERE timestamp < $timespan";
-            }
-        }
-        $count = serendipity_db_query($sql);
-        if(is_array($count)) {
+
+        $sql = "SELECT COUNT(1)
+                  FROM {$serendipity['dbPrefix']}$table";
+        $count = @serendipity_db_query($sql);
+        if (is_array($count)) {
             if (is_array($count[0])) {
                 return $count[0][0];
             }
         }
-        return 0;
+        return is_numeric($count[0]) ? $count[0] : '-';
     }
 
     function debugMsg($msg)
     {
         global $serendipity;
 
-        $this->debug_fp = @fopen ( $serendipity ['serendipityPath'] . 'templates_c/dbclean.log', 'a' );
+        $this->debug_fp = @fopen( $serendipity ['serendipityPath'] . 'templates_c/dbclean.log', 'a' );
         if (! $this->debug_fp) {
             return false;
         }
 
-        if (empty ( $msg )) {
-            fwrite ( $this->debug_fp, "failure \n" );
+        if (empty( $msg )) {
+            fwrite( $this->debug_fp, "failure \n" );
         } else {
-            fwrite ( $this->debug_fp, print_r ( $msg, true ) );
+            fwrite( $this->debug_fp, print_r( $msg, true ) );
         }
-        fclose ( $this->debug_fp );
+        fclose( $this->debug_fp );
     }
 
 }
