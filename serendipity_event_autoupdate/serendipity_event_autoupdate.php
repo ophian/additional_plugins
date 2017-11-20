f94520cb9b1fc91e3206294044933a5fe70b9029
Ian <aristophian@googlemail.com>
2017-11-20 08:53:12 +0100

    autoupdate: Minor fix to remove tags from hover help text

--- serendipity_event_autoupdate/serendipity_event_autoupdate.php
+++ serendipity_event_autoupdate/serendipity_event_autoupdate.php
@@ -18,7 +18,7 @@ class serendipity_event_autoupdate extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_AUTOUPDATE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'onli, Ian');
-        $propbag->add('version',       '1.5.2');
+        $propbag->add('version',       '1.5.3');
         $propbag->add('configuration', array('download_url', 'releasefile_url'));
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
@@ -182,7 +182,7 @@ class serendipity_event_autoupdate extends serendipity_event
                     $lang_char = LANG_CHARSET;
                     $ad_suite  = SERENDIPITY_ADMIN_SUITE;
                     $ad_title  = PLUGIN_AUTOUPD_MSG_TITLE;
-                    $ad_help   = PLUGIN_AUTOUPD_MSG_RELOAD;
+                    $ad_help   = strip_tags(strip_PLUGIN_AUTOUPD_MSG_RELOAD);
                     $ad_loader = !$nozipext ? '<div id="loader"><span></span><span></span><span></span></div>' : '';
                     $css_upd   = @file_get_contents(dirname(__FILE__) . '/upgrade.min.css');
                     $bimgpath  = $serendipity['serendipityHTTPPath'] . $serendipity['templatePath'] . 's9y_banner_small.png';
