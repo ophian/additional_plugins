e7f75f3212fe11e066e7c87516fe386260f5ce3c
Ian <aristophian@googlemail.com>
2018-04-27 10:01:01 +0200

    social: Sync - Update bundled shariff lib to 3.0.1

--- serendipity_event_social/serendipity_event_social.php
+++ serendipity_event_social/serendipity_event_social.php
@@ -18,7 +18,7 @@ class serendipity_event_social extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_SOCIAL_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'onli, Matthias Mees, Thomas Hochstein, Ian');
-        $propbag->add('version',       '0.15');
+        $propbag->add('version',       '0.16');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
         ));
