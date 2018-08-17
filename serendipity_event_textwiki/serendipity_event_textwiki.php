7930c5cdcd50e47d1c948d920fe90dbb3bdb20dd
Ian <aristophian@googlemail.com>
2018-08-17 16:31:23 +0200

    textwiki: Silence possible eventData property check

--- serendipity_event_textwiki/serendipity_event_textwiki.php
+++ serendipity_event_textwiki/serendipity_event_textwiki.php
@@ -406,7 +406,7 @@ class serendipity_event_textwiki extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_TEXTWIKI_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Tobias Schlitt');
-        $propbag->add('version',       '1.8');
+        $propbag->add('version',       '1.9');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
             'smarty'      => '2.6.7',
@@ -539,7 +539,7 @@ class serendipity_event_textwiki extends serendipity_event
                 case 'frontend_display':
                     foreach ($this->markup_elements AS $temp) {
                         if (serendipity_db_bool($this->get_config($temp['name'], true)) && isset($eventData[$temp['element']]) &&
-                            !$eventData['properties']['ep_disable_markup_' . $this->instance] &&
+                            @!$eventData['properties']['ep_disable_markup_' . $this->instance] &&
                             !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = $temp['element'];
                             $eventData[$element] = $this->wiki->transform($eventData[$element]);
