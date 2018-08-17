da67aa2f7ad9463dfd179bc92560c03907f7c4fd
Ian <aristophian@googlemail.com>
2018-08-17 16:30:05 +0200

    markdown: Silence possible eventData property check

--- serendipity_event_markdown/serendipity_event_markdown.php
+++ serendipity_event_markdown/serendipity_event_markdown.php
@@ -25,7 +25,7 @@ class serendipity_event_markdown extends serendipity_event
             'smarty'      => '2.6.7',
             'php'         => '5.3.0'
         ));
-        $propbag->add('version',       '1.27');
+        $propbag->add('version',       '1.28');
         $propbag->add('cachable_events', array('frontend_display' => true));
         $propbag->add('event_hooks',   array(
             'frontend_display' => true,
@@ -168,7 +168,7 @@ class serendipity_event_markdown extends serendipity_event
 
                     foreach ($this->markup_elements as $temp) {
                         if (serendipity_db_bool($this->get_config($temp['name'], true)) && !empty($eventData[$temp['element']]) &&
-                            !$eventData['properties']['ep_disable_markup_' . $this->instance] &&
+                            @!$eventData['properties']['ep_disable_markup_' . $this->instance] &&
                             !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = $temp['element'];
                             if ($mdv == 2) {
