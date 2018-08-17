ae134525bcc4bb626aec5f370a8f0ca8ab37afcd
Ian <aristophian@googlemail.com>
2018-08-17 16:30:34 +0200

    responsiveimage: Silence possible eventData property check

--- serendipity_event_responsiveimages/serendipity_event_responsiveimages.php
+++ serendipity_event_responsiveimages/serendipity_event_responsiveimages.php
@@ -18,7 +18,7 @@ class serendipity_event_responsiveimages extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_RESPONSIVE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Serendipity Team, Ian');
-        $propbag->add('version',       '0.5');
+        $propbag->add('version',       '0.6');
         $propbag->add('requirements',  array(
             'serendipity' => '2.3.1',
         ));
@@ -87,7 +87,7 @@ class serendipity_event_responsiveimages extends serendipity_event
                 case 'frontend_display':
                     foreach ($this->markup_elements AS $temp) {
                         if (serendipity_db_bool($this->get_config($temp['name'], 'true')) && isset($eventData[$temp['element']]) &&
-                            !$eventData['properties']['ep_disable_markup_' . $this->instance] &&
+                            @!$eventData['properties']['ep_disable_markup_' . $this->instance] &&
                             !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = $temp['element'];
                             // some fixes which could either be fixed by eg [trim($matches[1]) . " " . $srcset . " src=";] in _responsive_markup(),
