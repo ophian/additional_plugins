eef9e9c899cb17f3cf36ccd28238e328912b8786
Ian <aristophian@googlemail.com>
2018-08-31 09:08:55 +0200

    Fix 3a6775c

--- serendipity_plugin_heavyrotation/serendipity_plugin_heavyrotation.php
+++ serendipity_plugin_heavyrotation/serendipity_plugin_heavyrotation.php
@@ -7,9 +7,9 @@
  *
  * @author Lars Strojny <lars@strojny.net>
  */
-@define('SERENDIPITY_PLUGIN_HEAVYROTATION_VERSION', '0.11');
+@define('SERENDIPITY_PLUGIN_HEAVYROTATION_VERSION', '0.12');
 
-if (!defined('IN_serendipity_admin') || IN_serendipity !== true) die("Don't hack");
+if (!defined('IN_serendipity') || IN_serendipity !== true) die("Don't hack");
 
 if (version_compare(phpversion(), '5.1.0', '>=')) {
     @serendipity_plugin_api::load_language(dirname(__FILE__));
