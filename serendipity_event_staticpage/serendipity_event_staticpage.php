f09dfbbf940064b467571c1a4a27739bae77947a
Ian <aristophian@googlemail.com>
2019-01-09 21:14:32 +0100

    staticpage: Fix dtree id

--- serendipity_event_staticpage/serendipity_event_staticpage.php
+++ serendipity_event_staticpage/serendipity_event_staticpage.php
@@ -94,7 +94,7 @@ class serendipity_event_staticpage extends serendipity_event
         $propbag->add('page_configuration', $this->config);
         $propbag->add('type_configuration', $this->config_types);
         $propbag->add('author', 'Marco Rinck, Garvin Hicking, David Rolston, Falk Doering, Stephan Manske, Pascal Uhlmann, Ian, Don Chambers');
-        $propbag->add('version', '5.64');
+        $propbag->add('version', '5.65');
         $propbag->add('requirements', array(
             'serendipity' => '2.1.0',
             'smarty'      => '3.1.0',
