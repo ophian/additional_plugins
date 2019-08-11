dee75735828c0eb3324efa6f69513391361bfb8b
Ian <aristophian@googlemail.com>
2019-08-11 10:14:07 +0200

    contactform: Bump version for spartacus xml sync issue

--- serendipity_event_contactform/serendipity_event_contactform.php
+++ serendipity_event_contactform/serendipity_event_contactform.php
@@ -27,9 +27,9 @@ class serendipity_event_contactform extends serendipity_event
         $propbag->add('event_hooks',  array('entries_header' => true, 'entry_display' => true, 'genpage' => true));
         $propbag->add('configuration', array('permalink', 'pagetitle', 'backend_title', 'email', 'subject', 'counter', 'intro', 'sent', 'articleformat', 'dynamic_tpl', 'dynamic_fields', 'dynamic_fields_tpl', 'dynamic_fields_desc'));
         $propbag->add('author', 'Garvin Hicking, Ian Styx');
-        $propbag->add('version', '1.31');
+        $propbag->add('version', '1.32');
         $propbag->add('requirements',  array(
-            'serendipity' => '1.7',
+            'serendipity' => '1.7.0',
             'smarty'      => '3.1.0',
             'php'         => '4.1.0'
         ));
