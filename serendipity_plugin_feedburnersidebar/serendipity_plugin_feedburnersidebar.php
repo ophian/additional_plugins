7b8f4679a1bb1705ec42733942b4822c20493094
Ian <aristophian@googlemail.com>
2019-08-18 16:34:21 +0200

    feedburnersb: Sync [de] translation

--- serendipity_plugin_feedburnersidebar/serendipity_plugin_feedburnersidebar.php
+++ serendipity_plugin_feedburnersidebar/serendipity_plugin_feedburnersidebar.php
@@ -22,7 +22,7 @@ class serendipity_plugin_feedburnersidebar extends serendipity_plugin
         $propbag->add('description',   PLUGIN_FEEDBURNERSIDEBAR_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Aaron Axelsen');
-        $propbag->add('version',       '1.2');
+        $propbag->add('version',       '1.3');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
             'smarty'      => '2.6.7',
