54620f5bd4f3f52e1a90ddae0e9697077ec64f63
Ian <aristophian@googlemail.com>
2019-08-18 14:55:32 +0200

    linktrimmer: Partly Sync [de] translation

--- serendipity_event_linktrimmer/serendipity_event_linktrimmer.php
+++ serendipity_event_linktrimmer/serendipity_event_linktrimmer.php
@@ -22,7 +22,7 @@ class serendipity_event_linktrimmer extends serendipity_event
             'php'         => '5.2.0'
         ));
 
-        $propbag->add('version',       '1.7.1');
+        $propbag->add('version',       '1.7.2');
         $propbag->add('author',        'Garvin Hicking, Ian Styx');
         $propbag->add('stackable',     false);
         $propbag->add('configuration', array('prefix', 'frontpage', 'domain'));
