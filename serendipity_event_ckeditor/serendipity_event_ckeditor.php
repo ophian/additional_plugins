214acc21641c6f02cbf33d6b393ea8a570b37c1b
Ian <aristophian@googlemail.com>
2019-11-19 17:51:35 +0100

    ckeplus: Fix padding style of cke image toolbar button

--- serendipity_event_ckeditor/serendipity_event_ckeditor.php
+++ serendipity_event_ckeditor/serendipity_event_ckeditor.php
@@ -188,7 +188,7 @@ class serendipity_event_ckeditor extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_CKEDITOR_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Rustam Abdullaev, Ian Styx');
-        $propbag->add('version',       '4.13.0.3'); // is CKEDITOR Series 4.13.0 - and appended plugin revision .3
+        $propbag->add('version',       '4.13.0.4'); // is CKEDITOR Series 4.13.0 - and appended plugin revision .4
         $propbag->add('copyright',     'GPL or LGPL License');
         $propbag->add('requirements',  array(
             'serendipity' => '2.6.2',
@@ -423,7 +423,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function updateConfig()
     {
-        #$this->temporaryDowngrade('4.13.0.3', '4.13.0.2'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.13.0.4', '4.13.0.3'); // was temporary used for the harmonization of plugin and lib versions
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
             $this->set_config('last_'.$match[0].'_version', $match[1]);
@@ -437,7 +437,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function checkUpdate()
     {
-        #$this->temporaryDowngrade('4.13.0.3', '4.13.0.2'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.13.0.4', '4.13.0.3'); // was temporary used for the harmonization of plugin and lib versions
         $doupdate = false;
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
