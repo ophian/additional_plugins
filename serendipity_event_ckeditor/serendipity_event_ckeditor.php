b462bc98549440709de41fdd67381a154acc9fe7
Ian <aristophian@googlemail.com>
2019-12-15 15:59:38 +0100

    ckeplus: Upgrade to CKEDITOR 4.13.1

--- serendipity_event_ckeditor/serendipity_event_ckeditor.php
+++ serendipity_event_ckeditor/serendipity_event_ckeditor.php
@@ -49,7 +49,7 @@ class serendipity_event_ckeditor extends serendipity_event
      * @access protected
      * @var string
      */
-    protected $cke_zipfile = 'ckeditor_4.13.0.0-plus.zip';
+    protected $cke_zipfile = 'ckeditor_4.13.1.0-plus.zip';
 
     /**
      * Access property checkUpdateVersion
@@ -57,16 +57,16 @@ class serendipity_event_ckeditor extends serendipity_event
      * Verify release package versions - do update on upgrades!
      * @var array
      */
-    protected $checkUpdateVersion = array('ckeditor:4.13.0.0');
+    protected $checkUpdateVersion = array('ckeditor:4.13.1.0');
 
     /**
      * Access property revisionPackage
      * Note revisions of ckeditor and plugin additions to lang files
      * @var array
      */
-    protected $revisionPackage = array('CKEditor 4.13.0 (revision 83e9de8d6b, full package, 2019-09-27)',
+    protected $revisionPackage = array('CKEditor 4.13.1 (revision 411985373e, full package, 2019-12-09)',
                                        'CKEditor-Plugin: mediaembed, v. 0.6+ (https://github.com/frozeman/MediaEmbed, 2016-07-24)',
-                                       'CKEditor-Plugin: Manually added for current version "ajax", "autocomplete", "autogrow", "autolink", "clipboard", "codesnippet", "dialog", "dialogui", "embed", "embedbase", "embedsemantic", "emoji", "fakeobjects", "lineutils", "notification", "notificationaggregator", "placeholder", "textmatch", "textwatcher", "undo", "widget", "widgetselection" and "xml" plugins, 2019-09-27)',
+                                       'CKEditor-Plugin: Manually added for current version "ajax", "autocomplete", "autogrow", "autolink", "clipboard", "codesnippet", "dialog", "dialogui", "embed", "embedbase", "embedsemantic", "emoji", "fakeobjects", "lineutils", "notification", "notificationaggregator", "placeholder", "textmatch", "textwatcher", "undo", "widget", "widgetselection" and "xml" plugins, 2019-12-09)',
                                        'CKEditor-Plugin: procurator, v. 1.6 (Serendipity placeholder Plugin, 2016-01-01)',
                                        'CKEditor-Plugin: cheatsheet, v. 1.2 (Serendipity CKE-Cheatsheet Plugin, 2016-08-15)',
                                        'CKEditor-S9yCustomConfig, cke_config.js, v. 2.18, 2019-10-12',
@@ -188,7 +188,7 @@ class serendipity_event_ckeditor extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_CKEDITOR_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Rustam Abdullaev, Ian Styx');
-        $propbag->add('version',       '4.13.0.6'); // is CKEDITOR Series 4.13.0 - and appended plugin revision .6
+        $propbag->add('version',       '4.13.1.0'); // is CKEDITOR Series 4.13.1 - and appended plugin revision .0
         $propbag->add('copyright',     'GPL or LGPL License');
         $propbag->add('requirements',  array(
             'serendipity' => '2.6.2',
@@ -423,7 +423,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function updateConfig()
     {
-        #$this->temporaryDowngrade('4.13.0.6', '4.13.0.5'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.13.1.0', '4.13.0.6'); // was temporary used for the harmonization of plugin and lib versions
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
             $this->set_config('last_'.$match[0].'_version', $match[1]);
@@ -437,7 +437,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function checkUpdate()
     {
-        #$this->temporaryDowngrade('4.13.0.6', '4.13.0.5'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.13.1.0', '4.13.0.6'); // was temporary used for the harmonization of plugin and lib versions
         $doupdate = false;
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
