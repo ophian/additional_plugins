842a6df161e9cc14332bc4fae1931d8277162492
Ian <aristophian@googlemail.com>
2020-01-23 13:54:12 +0100

    analytics: Fix typos

--- serendipity_event_google_analytics/lang_de.inc.php
+++ serendipity_event_google_analytics/lang_de.inc.php
@@ -7,13 +7,13 @@
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_ADSENSE', 'AdSense Anzeigen verfolgen');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_ADSENSE_DESC', 'Überwacht AdSense Anzeigen und fügt die Daten GA hinzu. Dazu muß Analytics und AdSense verbunden werden. (->http://support.google.com/adsense/bin/answer.py?hl=de&answer=92625)');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_ANONYMIZEIP', 'IP Maskierung');
-@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_ANONYMIZEIP_DESC', 'Annonymisiert die IP der Besucher, indem Google das letzte Oktett der IP adresse nicht speichert. Verringert etwas die Genauigkeit der geographischen Berichte.');
+@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_ANONYMIZEIP_DESC', 'Anonymisiert die IP der Besucher, indem Google das letzte Oktett der IP Adresse nicht speichert. Verringert etwas die Genauigkeit der geographischen Berichte.');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_DOWNLOADS', 'Downloads verfolgen?');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_DOWNLOADS_DESC', 'Soll Google Analytics Downloads überwachen?');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_EXTERNAL', 'Externe Links verfolgen?');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_EXTERNAL_DESC', 'Soll Google Analytics externe Links überwachen?');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DOWNLOAD_EXTENSIONS', 'Welche Extensions sollen als Download überwacht werden?');
-@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DOWNLOAD_EXTENSIONS_DESC', 'Kommaseparierte Liste von Extensions.');
+@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DOWNLOAD_EXTENSIONS_DESC', 'Komma separierte Liste von Extensions.');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_INTERNAL_HOSTS', 'Hosts die du für dein Blog benutzt.');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_INTERNAL_HOSTS_DESC', 'Ein Host pro Zeile (www.example.net).');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_EXCLUDE_GROUPS', 'Zu ignorierende Benutzergruppe?');
--- serendipity_event_google_analytics/lang_en.inc.php
+++ serendipity_event_google_analytics/lang_en.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_NAME', 'Google Analytics');
-@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DESC', 'This plugin adds extendet Google Analytic funcionality to youre Page like tracking external links or download links.');
+@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DESC', 'This plugin adds extended Google Analytic functionality to your Page like tracking external links or download links.');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_ACCOUNT_NUMBER', 'Google Analytics account number');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_ACCOUNT_NUMBER_DESC', 'Your Google Analytics account number. The xxxx-x of _uacct="UA-xxxx-x";');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_ADSENSE', 'Track AdSense Ads');
@@ -13,7 +13,7 @@
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_EXTERNAL', 'Track external links?');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_TRACK_EXTERNAL_DESC', '');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DOWNLOAD_EXTENSIONS', 'Which downloads should be tracked?');
-@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DOWNLOAD_EXTENSIONS_DESC', 'Comma separeted list of tracked extensions.');
+@define('PLUGIN_EVENT_GOOGLE_ANALYTICS_DOWNLOAD_EXTENSIONS_DESC', 'Comma separated list of tracked extensions.');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_INTERNAL_HOSTS', 'Hosts you use for your blog.');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_INTERNAL_HOSTS_DESC', 'One host per line (www.example.net).');
 @define('PLUGIN_EVENT_GOOGLE_ANALYTICS_EXCLUDE_GROUPS', 'Which usergroups should not be tracked?');
