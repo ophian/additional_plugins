d560af35aba07c75960a08a572d749cc5102053e
Ian <aristophian@googlemail.com>
2020-07-19 16:52:04 +0200

    statistics: Fix a possible column dislay mismatch with e661dd20bbb55a78f508af0b62fd1d8c02c70667

--- serendipity_event_statistics/serendipity_event_statistics.php
+++ serendipity_event_statistics/serendipity_event_statistics.php
@@ -18,7 +18,7 @@ class serendipity_event_statistics extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_STATISTICS_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Arnan de Gans, Garvin Hicking, Fredrik Sandberg, kalkin, Matthias Mees, Ian Styx');
-        $propbag->add('version',       '2.01');
+        $propbag->add('version',       '2.02');
         $propbag->add('requirements',  array(
             'serendipity' => '2.9',
             'php'         => '7.2'
@@ -544,6 +544,7 @@ class serendipity_event_statistics extends serendipity_event
 .serendipity_statistics:not(.extended_statistics) {
     column-count: 2;
     width: 100%;
+    display: block;
 }
 .serendipity_statistics:not(.extended_statistics) section {
     width: 100%;
