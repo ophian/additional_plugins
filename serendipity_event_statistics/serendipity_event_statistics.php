2f7189c1dd63d00cf952b8ef386e654dc2294007
Ian <aristophian@googlemail.com>
2020-07-24 10:20:56 +0200

    statistics: Retry fixing column display mismatch by ff8a3b2cd447b6f12a17a4f550bdfedb7237c872 ff

--- serendipity_event_statistics/serendipity_event_statistics.php
+++ serendipity_event_statistics/serendipity_event_statistics.php
@@ -18,7 +18,7 @@ class serendipity_event_statistics extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_STATISTICS_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Arnan de Gans, Garvin Hicking, Fredrik Sandberg, kalkin, Matthias Mees, Ian Styx');
-        $propbag->add('version',       '2.03');
+        $propbag->add('version',       '2.04');
         $propbag->add('requirements',  array(
             'serendipity' => '2.9',
             'php'         => '7.2'
@@ -544,7 +544,7 @@ class serendipity_event_statistics extends serendipity_event
 #content .serendipity_statistics:not(.extended_statistics) {
     column-count: 2;
     column-gap: .5em;
-    width: auto;
+    width: 99%;
     display: block;
 }
 #content .serendipity_statistics:not(.extended_statistics) section {
