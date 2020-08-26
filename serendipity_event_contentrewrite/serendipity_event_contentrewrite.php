2acfcf042ba6ceef48c03d4de502b28be4c1258a
Ian <aristophian@googlemail.com>
2020-08-26 17:06:19 +0200

    contentrewrite: Minor consistency fix

--- serendipity_event_contentrewrite/serendipity_event_contentrewrite.php
+++ serendipity_event_contentrewrite/serendipity_event_contentrewrite.php
@@ -265,7 +265,7 @@ class serendipity_event_contentrewrite extends serendipity_event
                             $regex = '°([^\d\w])(' . preg_quote($v_from) . ')([^\d\w])°msU';
 
                             // \1 and \3 are the prepend/append strings (usually whitespaces) and $new containing the new value
-                            foreach ($this->markup_elements as $temp) {
+                            foreach ($this->markup_elements AS $temp) {
                                 if (serendipity_db_bool($this->get_config($temp['name'], true)) && isset($eventData[$temp['element']]) &&
                                     @!$eventData['properties']['ep_disable_markup_' . $this->instance] &&
                                     !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
