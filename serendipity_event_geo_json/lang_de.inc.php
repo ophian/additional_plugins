92a9cf1a22c896857096f1634cd2fd6cf5dba887
Ian <aristophian@googlemail.com>
2020-09-17 16:05:12 +0200

    geo_json: [Sync S9y] - Create geo_json plugin

---
+++ serendipity_event_geo_json/lang_de.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+@define('PLUGIN_EVENT_GEO_JSON_NAME', 'Geo-JSON');
+@define('PLUGIN_EVENT_GEO_JSON_DESC', 'Erstellt ein JSON-Object mit allen Geo-Daten, welches mit JavaScript verarbeitet werden kann.');
+
