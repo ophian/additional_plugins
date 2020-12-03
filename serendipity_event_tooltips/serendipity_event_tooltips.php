707ed3b15d8ac298dc9633442e02235931e8c578
Ian <aristophian@googlemail.com>
2020-12-03 10:51:29 +0100

    tooltip: Fix and improve markup_element check for consistency

--- serendipity_event_tooltips/serendipity_event_tooltips.php
+++ serendipity_event_tooltips/serendipity_event_tooltips.php
@@ -18,8 +18,8 @@ class serendipity_event_tooltips extends serendipity_event
         $propbag->add('name',          PLUGIN_EVENT_TOOLTIPS_NAME);
         $propbag->add('description',   PLUGIN_EVENT_TOOLTIPS_DESC);
         $propbag->add('stackable',     false);
-        $propbag->add('author',        'Enrico Stahn');
-        $propbag->add('version',       '1.8');
+        $propbag->add('author',        'Enrico Stahn, Ian Styx');
+        $propbag->add('version',       '1.9');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
             'smarty'      => '3.1.6',
@@ -283,10 +283,10 @@ class serendipity_event_tooltips extends serendipity_event
                     break;
 
                 case 'frontend_display':
-                    foreach ($this->markup_elements as $temp) {
-                        if (serendipity_db_bool($this->get_config($temp['name'], true)) && isset($eventData[$temp['element']]) &&
-                            @!$eventData['properties']['ep_disable_markup_' . $this->instance] &&
-                            !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
+                    foreach ($this->markup_elements AS $temp) {
+                        if (serendipity_db_bool($this->get_config($temp['name'], 'true')) && !empty($eventData[$temp['element']])
+                        &&  (!isset($eventData['properties']['ep_disable_markup_' . $this->instance]) || !$eventData['properties']['ep_disable_markup_' . $this->instance])
+                        &&  !isset($serendipity['POST']['properties']['disable_markup_' . $this->instance])) {
                             $element = &$eventData[$temp['element']];
 
                             if (serendipity_db_bool($this->get_config('fullimages', true))) {
