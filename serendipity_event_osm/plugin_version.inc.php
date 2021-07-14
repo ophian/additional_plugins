6476f2e4163f78635208ec66edaba2cb195d6e9d
Ian <aristophian@googlemail.com>
2021-07-14 10:36:04 +0200

    osm: Update OpenLayers lib to v6.6.0

--- serendipity_event_osm/plugin_version.inc.php
+++ serendipity_event_osm/plugin_version.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-@define('PLUGIN_EVENT_OSM_VERSION',	'0.17');
+@define('PLUGIN_EVENT_OSM_VERSION',	'0.18');
 @define('PLUGIN_EVENT_OSM_AUTHOR', 	'Martin Sewelies');
 
--- serendipity_event_osm/resources/osm.js
+++ serendipity_event_osm/resources/osm.js
@@ -162,4 +162,4 @@ window.addEventListener("load", () => {
             });
         });
     });
-};
+});
