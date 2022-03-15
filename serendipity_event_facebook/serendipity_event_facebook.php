cbb56b80985e96d017839acd66f967aecf5f6b35
Ian <aristophian@googlemail.com>
2022-03-15 15:30:01 +0100

    facebook: Follow Styx DB fixes for index key length conditioning
    on install up from PHP 8.0.16 versions

--- serendipity_event_facebook/serendipity_event_facebook.php
+++ serendipity_event_facebook/serendipity_event_facebook.php
@@ -24,7 +24,7 @@ class serendipity_event_facebook extends serendipity_event
             'smarty'      => '3.1',
             'php'         => '7.3'
         ));
-        $propbag->add('version',       '2.00');
+        $propbag->add('version',       '2.1.0');
         $propbag->add('groups', array('FRONTEND_VIEWS'));
         $propbag->add('event_hooks', array(
             'frontend_display'  => true,
@@ -87,17 +87,27 @@ class serendipity_event_facebook extends serendipity_event
 
             if ($serendipity['dbType'] == 'mysqli') {
                 $serendipity['db_server_info'] = $serendipity['db_server_info'] ?? mysqli_get_server_info($serendipity['dbConn']); // eg.  == 5.5.5-10.4.11-MariaDB
+                // be a little paranoid...
+                if (substr($serendipity['db_server_info'], 0, 6) === '5.5.5-') {
+                    // strip any possible added prefix having this 5.5.5 version string (which was never released). PHP up from 8.0.16 now strips it correctly.
+                    $serendipity['db_server_info'] = str_replace('5.5.5-', '', $serendipity['db_server_info']);
+                }
+                $db_version_match = explode('-', $serendipity['db_server_info']);
                 if (stristr(strtolower($serendipity['db_server_info']), 'mariadb')) {
-                    $db_version_match = explode('-', $serendipity['db_server_info']);
-                    if (version_compare($db_version_match[1], '10.5.0', '>=')) {
+                    if (version_compare($db_version_match[0], '10.5.0', '>=')) {
                         $q = "CREATE INDEX fbindex ON {$serendipity['dbPrefix']}facebook (base_url);";
-                    } elseif (version_compare($db_version_match[1], '10.3.0', '>=')) {
+                    } elseif (version_compare($db_version_match[0], '10.3.0', '>=')) {
                         $q = "CREATE INDEX fbindex ON {$serendipity['dbPrefix']}facebook (base_url(250));"; // max key 1000 bytes
                     } else {
                         $q = "CREATE INDEX fbindex ON {$serendipity['dbPrefix']}facebook (base_url(191));"; // 191 - old MyISAMs
                     }
                 } else {
-                    $q = "CREATE INDEX fbindex ON {$serendipity['dbPrefix']}facebook (base_url(191));"; // Oracle Mysql/InnoDB max key 767 bytes
+                    // Oracle MySQL - https://dev.mysql.com/doc/refman/5.7/en/innodb-limits.html
+                    if (version_compare($db_version_match[0], '5.7.7', '>=')) {
+                        $q = "CREATE INDEX fbindex ON {$serendipity['dbPrefix']}facebook (base_url);"; // Oracle Mysql/InnoDB max key up to 3072 bytes
+                    } else {
+                        $q = "CREATE INDEX fbindex ON {$serendipity['dbPrefix']}facebook (base_url(191));"; // Oracle Mysql/InnoDB max key 767 bytes
+                    }
                 }
                 serendipity_db_schema_import($q);
             } else {
