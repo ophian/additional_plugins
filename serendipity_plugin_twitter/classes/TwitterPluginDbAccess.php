2f126f6dbdf4bdfaed322209bb42b2445126e108
Ian <aristophian@googlemail.com>
2022-03-15 15:37:41 +0100

    twitter: Follow Styx DB fixes for index key length conditioning
    on install up from PHP 8.0.16 versions

--- serendipity_plugin_twitter/classes/TwitterPluginDbAccess.php
+++ serendipity_plugin_twitter/classes/TwitterPluginDbAccess.php
@@ -144,16 +144,21 @@ class TwitterPluginDbAccess
         if (!TwitterPluginDbAccess::table_created('tweetbackshorturls')) {
             if ($serendipity['dbType'] == 'mysqli') {
                 $serendipity['db_server_info'] = $serendipity['db_server_info'] ?? mysqli_get_server_info($serendipity['dbConn']); // eg.  == 5.5.5-10.4.11-MariaDB
+                // be a little paranoid...
+                if (substr($serendipity['db_server_info'], 0, 6) === '5.5.5-') {
+                    // strip any possible added prefix having this 5.5.5 version string (which was never released). PHP up from 8.0.16 now strips it correctly.
+                    $serendipity['db_server_info'] = str_replace('5.5.5-', '', $serendipity['db_server_info']);
+                }
+                $db_version_match = explode('-', $serendipity['db_server_info']);
                 if (stristr(strtolower($serendipity['db_server_info']), 'mariadb')) {
-                    $db_version_match = explode('-', $serendipity['db_server_info']);
-                    if (version_compare($db_version_match[1], '10.5.0', '>=')) {
+                    if (version_compare($db_version_match[0], '10.5.0', '>=')) {
                         $q = "CREATE TABLE {$serendipity['dbPrefix']}tweetbackshorturls (" .
                                 "service varchar(15) not null, " .
                                 "longurl varchar(255) not null, " .
                                 "shorturl varchar(50) not null, " .
                                 "PRIMARY KEY (service, longurl)" .
                             ")";
-                    } elseif (version_compare($db_version_match[1], '10.3.0', '>=')) {
+                    } elseif (version_compare($db_version_match[0], '10.3.0', '>=')) {
                          $q = "CREATE TABLE {$serendipity['dbPrefix']}tweetbackshorturls (" .
                                 "service varchar(15) not null, " .
                                 "longurl varchar(255) not null, " .
@@ -169,12 +174,22 @@ class TwitterPluginDbAccess
                             ")"; // 191 - 15 - old MyISAMs
                     }
                 } else {
-                    $q = "CREATE TABLE {$serendipity['dbPrefix']}tweetbackshorturls (" .
-                            "service varchar(15) not null, " .
-                            "longurl varchar(255) not null, " .
-                            "shorturl varchar(50) not null, " .
-                            "PRIMARY KEY (service, longurl(176))" .
-                        ")"; // Oracle Mysql/InnoDB max key 767 bytes
+                    // Oracle MySQL - https://dev.mysql.com/doc/refman/5.7/en/innodb-limits.html
+                    if (version_compare($db_version_match[0], '5.7.7', '>=')) {
+                        $q = "CREATE TABLE {$serendipity['dbPrefix']}tweetbackshorturls (" .
+                                "service varchar(15) not null, " .
+                                "longurl varchar(255) not null, " .
+                                "shorturl varchar(50) not null, " .
+                                "PRIMARY KEY (service, longurl)" .
+                            ")"; // Oracle Mysql/InnoDB max key up to 3072 bytes
+                    } else {
+                        $q = "CREATE TABLE {$serendipity['dbPrefix']}tweetbackshorturls (" .
+                                "service varchar(15) not null, " .
+                                "longurl varchar(255) not null, " .
+                                "shorturl varchar(50) not null, " .
+                                "PRIMARY KEY (service, longurl(176))" .
+                            ")"; // Oracle Mysql/InnoDB max key 767 bytes
+                    }
                 }
             } else {
                 $q = "CREATE TABLE {$serendipity['dbPrefix']}tweetbackshorturls (" .
@@ -192,17 +207,27 @@ class TwitterPluginDbAccess
 
             if ($serendipity['dbType'] == 'mysqli') {
                 $serendipity['db_server_info'] = $serendipity['db_server_info'] ?? mysqli_get_server_info($serendipity['dbConn']); // eg.  == 5.5.5-10.4.11-MariaDB
+                // be a little paranoid...
+                if (substr($serendipity['db_server_info'], 0, 6) === '5.5.5-') {
+                    // strip any possible added prefix having this 5.5.5 version string (which was never released). PHP up from 8.0.16 now strips it correctly.
+                    $serendipity['db_server_info'] = str_replace('5.5.5-', '', $serendipity['db_server_info']);
+                }
+                $db_version_match = explode('-', $serendipity['db_server_info']);
                 if (stristr(strtolower($serendipity['db_server_info']), 'mariadb')) {
-                    $db_version_match = explode('-', $serendipity['db_server_info']);
-                    if (version_compare($db_version_match[1], '10.5.0', '>=')) {
+                    if (version_compare($db_version_match[0], '10.5.0', '>=')) {
                         $q = "CREATE INDEX idx_tweetbackshorturls_longurl ON {$serendipity['dbPrefix']}tweetbackshorturls (longurl);";
-                    } elseif (version_compare($db_version_match[1], '10.3.0', '>=')) {
+                    } elseif (version_compare($db_version_match[0], '10.3.0', '>=')) {
                         $q = "CREATE INDEX idx_tweetbackshorturls_longurl ON {$serendipity['dbPrefix']}tweetbackshorturls (longurl(250));"; // max key 1000 bytes
                     } else {
                         $q = "CREATE INDEX idx_tweetbackshorturls_longurl ON {$serendipity['dbPrefix']}tweetbackshorturls (longurl(191));"; // 191 - old MyISAMs
                     }
                 } else {
-                    $q = "CREATE INDEX idx_tweetbackshorturls_longurl ON {$serendipity['dbPrefix']}tweetbackshorturls (longurl(191));"; // Oracle Mysql/InnoDB max key 767 bytes
+                    // Oracle MySQL - https://dev.mysql.com/doc/refman/5.7/en/innodb-limits.html
+                    if (version_compare($db_version_match[0], '5.7.7', '>=')) {
+                        $q = "CREATE INDEX idx_tweetbackshorturls_longurl ON {$serendipity['dbPrefix']}tweetbackshorturls (longurl);"; // Oracle Mysql/InnoDB max key up to 3072 bytes
+                    } else {
+                        $q = "CREATE INDEX idx_tweetbackshorturls_longurl ON {$serendipity['dbPrefix']}tweetbackshorturls (longurl(191));"; // Oracle Mysql/InnoDB max key 767 bytes
+                    }
                 }
                 serendipity_db_schema_import($q);
             } else {
--- serendipity_plugin_twitter/plugin_version.inc.php
+++ serendipity_plugin_twitter/plugin_version.inc.php
@@ -1,4 +1,4 @@
 <?php
 
 // Actual version of both plugins
-@define('PLUGIN_TWITTER_VERSION', '2.00');
+@define('PLUGIN_TWITTER_VERSION', '2.1.0');
