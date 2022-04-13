c765b4187268c0ae50123e09cdaa13407a9ba389
Ian <aristophian@googlemail.com>
2022-04-13 10:02:02 +0200

    lightbox: Fix empty_dir() method for very old plugin instances

--- serendipity_event_lightbox/serendipity_event_lightbox.php
+++ serendipity_event_lightbox/serendipity_event_lightbox.php
@@ -24,7 +24,7 @@ class serendipity_event_lightbox extends serendipity_event
         $propbag->add('name',           PLUGIN_EVENT_LIGHTBOX_NAME);
         $propbag->add('description',    PLUGIN_EVENT_LIGHTBOX_DESC);
         $propbag->add('author',         'Thomas Nesges, Andy Hopkins, Lokesh Dhakar, Cody Lindley, Stephan Manske, Grischa Brockhaus, Ian Styx');
-        $propbag->add('version',        '2.11.0');
+        $propbag->add('version',        '2.11.1');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
             'php'         => '5.6.0'
@@ -403,7 +403,7 @@ class serendipity_event_lightbox extends serendipity_event
                 @rmdir($file->__toString());
             }
         }
-        @rmdir(dir);
+        @rmdir($dir);
     }
 
 }
