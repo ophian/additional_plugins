314c07b9f7537918cc28b2a0ff4b38329dda934e
Ian <aristophian@googlemail.com>
2022-07-27 13:37:03 +0200

    autoupdate: Fix PHP fcgid error thown by 69b0472592a45ae9e62fa604674b9adbe6b860c1 reload error fix

--- serendipity_event_autoupdate/serendipity_event_autoupdate.php
+++ serendipity_event_autoupdate/serendipity_event_autoupdate.php
@@ -16,7 +16,7 @@ class serendipity_event_autoupdate extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_AUTOUPDATE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'onli, Ian Styx');
-        $propbag->add('version',       '1.9.1');
+        $propbag->add('version',       '1.9.2');
         $propbag->add('configuration', array('download_url', 'releasefile_url', 'purge_zips'));
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
@@ -398,7 +398,7 @@ EOS;
                 $done = @copy($url, $update) ? true : false;
                 sleep(1);
             }
-            @$zip->close();
+            $zip->close();
         } else {
             $done = @copy($url, $update) ? true : false;
             sleep(1);
