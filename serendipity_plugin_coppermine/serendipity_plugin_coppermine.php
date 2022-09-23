f50daa997fac7e9e227567eaca14d807e287f6e0
Ian <aristophian@googlemail.com>
2022-09-23 11:51:38 +0200

    coppermine: Fix & UP for consistency

--- serendipity_plugin_coppermine/serendipity_plugin_coppermine.php
+++ serendipity_plugin_coppermine/serendipity_plugin_coppermine.php
@@ -8,17 +8,10 @@ if (IN_serendipity !== true) {
     die ("Don't hack!");
 }
 
-// Probe for a language include with constants. Still include defines later on, if some constants were missing
-$probelang = dirname(__FILE__) . '/' . $serendipity['charset'] . 'lang_' . $serendipity['lang'] . '.inc.php';
-if (file_exists($probelang)) {
-    include $probelang;
-}
-
-include_once dirname(__FILE__) . '/lang_en.inc.php';
+@serendipity_plugin_api::load_language(dirname(__FILE__));
 
 class serendipity_plugin_coppermine extends serendipity_plugin
 {
-
     var $title = PLUGIN_CPGS_NAME;
 
 	function introspect(&$propbag)
@@ -30,8 +23,8 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 
         $propbag->add('stackable',     true);
         $propbag->add('author',        'Matthew Maude');
-        $propbag->add('version',       '1.4');
-        $propbag->add('requirements',  array('serendipity' => '0.8'));
+        $propbag->add('version',       '1.5');
+        $propbag->add('requirements',  array('serendipity' => '2.0'));
         $propbag->add('groups', array('IMAGES'));
 		$propbag->add('configuration', array('cm_server','cm_prefix','cm_path','cm_title','cm_db','cm_user','cm_pass','cm_size','cm_count','cm_type','cm_plugin_title','cm_albumlink','cm_album','cm_gallerypath','cm_resolve'));
 
@@ -39,122 +32,121 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 
 	}
 
-	function introspect_config_item($name, &$propbag){
-
+	function introspect_config_item($name, &$propbag)
+    {
 		switch($name) {
 
-		case 'cm_resolve':
-			$propbag->add('type',        'boolean');
-			$propbag->add('name',        PLUGIN_CPGS_THUMB_NAME);
-			$propbag->add('description', PLUGIN_CPGS_THUMB_DESC);
-			$propbag->add('default',     'true');
-			break;
+            case 'cm_resolve':
+                $propbag->add('type',        'boolean');
+                $propbag->add('name',        PLUGIN_CPGS_THUMB_NAME);
+                $propbag->add('description', PLUGIN_CPGS_THUMB_DESC);
+                $propbag->add('default',     'true');
+                break;
 
-		case 'cm_type':
-			$propbag->add('type', 'radio');
-			$propbag->add('name', PLUGIN_CPGS_TYPE_NAME);
-			$propbag->add('description', PLUGIN_CPGS_TYPE_DESC);
-			$propbag->add('radio',  array(
-				'value' => array('recent', 'random', 'popular'),
-				'desc'  => array(PLUGIN_CPGS_RECENT, PLUGIN_CPGS_RANDOM, PLUGIN_CPGS_POPULAR)
-				));
-			$propbag->add('default', 'recent');
-			break;
+            case 'cm_type':
+                $propbag->add('type', 'radio');
+                $propbag->add('name', PLUGIN_CPGS_TYPE_NAME);
+                $propbag->add('description', PLUGIN_CPGS_TYPE_DESC);
+                $propbag->add('radio',  array(
+                    'value' => array('recent', 'random', 'popular'),
+                    'desc'  => array(PLUGIN_CPGS_RECENT, PLUGIN_CPGS_RANDOM, PLUGIN_CPGS_POPULAR)
+                    ));
+                $propbag->add('default', 'recent');
+                break;
 
-		case 'cm_plugin_title':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_TITLE_NAME);
-			$propbag->add('description', PLUGIN_CPGS_TITLE_DESC);
-			$propbag->add('default', 'My Images');
-			break;
+            case 'cm_plugin_title':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_TITLE_NAME);
+                $propbag->add('description', PLUGIN_CPGS_TITLE_DESC);
+                $propbag->add('default', 'My Images');
+                break;
 
+            case 'cm_server':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_SERVER_NAME);
+                $propbag->add('description', PLUGIN_CPGS_SERVER_DESC);
+                $propbag->add('default', 'localhost');
+                break;
 
-		case 'cm_server':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_SERVER_NAME);
-			$propbag->add('description', PLUGIN_CPGS_SERVER_DESC);
-			$propbag->add('default', 'localhost');
-			break;
+            case 'cm_db':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_DB_NAME);
+                $propbag->add('description', PLUGIN_CPGS_DB_DESC);
+                $propbag->add('default', 'coppermine');
+                break;
 
-		case 'cm_db':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_DB_NAME);
-			$propbag->add('description', PLUGIN_CPGS_DB_DESC);
-			$propbag->add('default', 'coppermine');
-			break;
+            case 'cm_prefix':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_PREFIX_NAME);
+                $propbag->add('description', PLUGIN_CPGS_PREFIX_DESC);
+                $propbag->add('default', 'cpg132_');
+                break;
 
-		case 'cm_prefix':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_PREFIX_NAME);
-			$propbag->add('description', PLUGIN_CPGS_PREFIX_DESC);
-			$propbag->add('default', 'cpg132_');
-			break;
+            case 'cm_user':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_USER_NAME);
+                $propbag->add('description', PLUGIN_CPGS_USER_DESC);
+                $propbag->add('default', 'username');
+                break;
 
-		case 'cm_user':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_USER_NAME);
-			$propbag->add('description', PLUGIN_CPGS_USER_DESC);
-			$propbag->add('default', 'username');
-			break;
+            case 'cm_pass':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_PASSWORD_NAME);
+                $propbag->add('description', PLUGIN_CPGS_PASSWORD_DESC);
+                $propbag->add('default', 'password');
+                break;
 
-		case 'cm_pass':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_PASSWORD_NAME);
-			$propbag->add('description', PLUGIN_CPGS_PASSWORD_DESC);
-			$propbag->add('default', 'password');
-			break;
+            case 'cm_path':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_URL_NAME);
+                $propbag->add('description', PLUGIN_CPGS_URL_DESC);
+                $propbag->add('default', 'http://gallery.com');
+                break;
 
-		case 'cm_path':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_URL_NAME);
-			$propbag->add('description', PLUGIN_CPGS_URL_DESC);
-			$propbag->add('default', 'http://gallery.com');
-			break;
+            case 'cm_gallerypath':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_GALLLINK_NAME);
+                $propbag->add('description', PLUGIN_CPGS_GALLLINK_DESC);
+                $propbag->add('default', 'http://gallery.com');
+                break;
 
-		case 'cm_gallerypath':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_GALLLINK_NAME);
-			$propbag->add('description', PLUGIN_CPGS_GALLLINK_DESC);
-			$propbag->add('default', 'http://gallery.com');
-			break;
+            case 'cm_title':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_GALLNAME_NAME);
+                $propbag->add('description', PLUGIN_CPGS_GALLNAME_DESC);
+                $propbag->add('default', 'More images...');
+                break;
 
-		case 'cm_title':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_GALLNAME_NAME);
-			$propbag->add('description', PLUGIN_CPGS_GALLNAME_DESC);
-			$propbag->add('default', 'More images...');
-			break;
+            case 'cm_count':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_COUNT_NAME);
+                $propbag->add('description', PLUGIN_CPGS_COUNT_DESC);
+                $propbag->add('default', '4');
+                break;
 
-		case 'cm_count':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_COUNT_NAME);
-			$propbag->add('description', PLUGIN_CPGS_COUNT_DESC);
-			$propbag->add('default', '4');
-			break;
+            case 'cm_size':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_SIZE_NAME);
+                $propbag->add('description', PLUGIN_CPGS_SIZE_DESC);
+                $propbag->add('default', '100');
+                break;
 
-		case 'cm_size':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_SIZE_NAME);
-			$propbag->add('description', PLUGIN_CPGS_SIZE_DESC);
-			$propbag->add('default', '100');
-			break;
+            case 'cm_album':
+                $propbag->add('type', 'string');
+                $propbag->add('name', PLUGIN_CPGS_FILTER_NAME);
+                $propbag->add('description', PLUGIN_CPGS_FILTER_DESC);
+                $propbag->add('default', '');
+                break;
 
-		case 'cm_album':
-			$propbag->add('type', 'string');
-			$propbag->add('name', PLUGIN_CPGS_FILTER_NAME);
-			$propbag->add('description', PLUGIN_CPGS_FILTER_DESC);
-			$propbag->add('default', '');
-			break;
+            case 'cm_albumlink':
+                $propbag->add('type', 'boolean');
+                $propbag->add('name', PLUGIN_CPGS_ALBUM_NAME);
+                $propbag->add('description', PLUGIN_CPGS_ALBUM_DESC);
+                $propbag->add('default', true);
+                break;
 
-		case 'cm_albumlink':
-			$propbag->add('type', 'boolean');
-			$propbag->add('name', PLUGIN_CPGS_ALBUM_NAME);
-			$propbag->add('description', PLUGIN_CPGS_ALBUM_DESC);
-			$propbag->add('default', true);
-			break;
-
-		default:
-			return false;
+            default:
+                return false;
 
 		}
 
@@ -162,11 +154,11 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 
 	}
 
-	function generate_content(&$title){
+	function generate_content(&$title)
+    {
+		global $serendipity;
 
 		$title = $this->get_config('cm_plugin_title');
-
-		global $serendipity;
 
 		$dbserver = $this->get_config('cm_server');
 		$prefix = $this->get_config('cm_prefix');
@@ -188,35 +180,27 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 
 		$filter = '';
 
-
-		if (!empty($album)){
+		if (!empty($album)) {
 
 			$show = array();
 			$hide = array();
 
-			$ids = preg_split('/[\s,]/',$album,-1,PREG_SPLIT_NO_EMPTY);
-			foreach ($ids as $id){
-
-				if (preg_match('/^!/',$id) && is_numeric(substr($id,1))){
-
+			$ids = preg_split('/[\s,]/', $album, -1, PREG_SPLIT_NO_EMPTY);
+			foreach ($ids AS $id) {
+				if (preg_match('/^!/',$id) && is_numeric(substr($id,1))) {
 					$hide[] = substr($id,1);
-
-				} else if (is_numeric($id)){
-
+				} else if (is_numeric($id)) {
 					$show[] = $id;
-
 				}
-
 			}
 
-			if (count($show) || count($hide)){
-
+			if (count($show) || count($hide)) {
 				$filter = ' WHERE aid ';
 
-				if (count($show)){
+				if (count($show)) {
 					$filter .= 'IN (';
 					$c = 0;
-					foreach ($show as $id){
+					foreach ($show AS $id) {
 						$filter .= $id;
 						if ($c < count($show)-1) $filter .= ',';
 						++$c;
@@ -225,41 +209,35 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 					if (count($hide)) $filter .= ' AND aid ';
 				}
 
-				if (count($hide)){
+				if (count($hide)) {
 					$filter .= 'NOT IN (';
 					$c = 0;
-					foreach ($hide as $id){
+					foreach ($hide AS $id) {
 						$filter .= $id;
 						if ($c < count($hide)-1) $filter .= ',';
 						++$c;
 					}
 					$filter .= ') ';
 				}
-
 			}
-
 		}
 
 		switch($type) {
+            case 'random':
+                $order = 'RAND()';
+                break;
 
-		case 'random':
-			$order = 'RAND()';
-			break;
+            case 'popular':
+                $order = 'hits DESC';
+                break;
 
-		case 'popular':
-			$order = 'hits DESC';
-			break;
-
-		case 'recent':
-		default:
-			$order = 'pid DESC';
-			break;
-
+            case 'recent':
+            default:
+                $order = 'pid DESC';
+                break;
 		}
 
-
 		//if ($cm_title != "") $title = $cm_title;
-
 
 		$DB = @mysql_connect($dbserver, $dbuser, $dbpass);
 
@@ -277,7 +255,7 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 		$query = "SELECT * FROM " . $prefix	. "pictures " . $filter . " ORDER BY " . $order . " LIMIT 0," . $count;
 
 		$result = @mysql_query($query,$DB);
-		if (!$result){
+		if (!$result) {
 			echo 'Error performing query: ' . mysql_error() . '<br>';
 			$this->finished($DB);
 			return;
@@ -304,21 +282,21 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 
 		$defthumbnail = $serendipity['serendipityHTTPPath'].'plugins/serendipity_plugin_coppermine/none.jpg';
 
-		if ($resolve){
+		if ($resolve) {
 			$file = @fopen($path.'/images/thumb_nopic.jpg',"r");
-			if ($file){
+			if ($file) {
 				fclose($file);
 				$defthumbnail = $path.'/images/thumb_nopic.jpg';
 			} else {
 				$file = @fopen($path.'/images/nopic.jpg',"r");
-				if ($file){
+				if ($file) {
 					fclose($file);
 					$defthumbnail = $path.'/images/nopic.jpg';
 				}
 			}
 		}
 
-		while ( $row = mysql_fetch_array($result) ) {
+		while ($row = mysql_fetch_array($result)) {
 
 			echo '<div class="cm_cell">
 				';
@@ -330,19 +308,19 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 			$height = $row['pheight'];
 
 			if (!isset($row['pwidth']) || empty($row['pwidth'])	|| $row['pwidth'] <= 0 ||
-				!isset($row['pheight']) || empty($row['pheight']) || $row['pheight'] <= 0){
+				!isset($row['pheight']) || empty($row['pheight']) || $row['pheight'] <= 0) {
 
 				$defimage = '<a href="' . $path . '/displayimage.php?album=' . $albumid . '&pos=-' . $pos . '">' .
 					'<img src="' . $defthumbnail . '" title="' . $row['filename'] . '" alt="' . $row['filename'] . '" class="cm_thumbnail" style="width:' . $size . 'px; height:' . $size . 'px;"></a>';
 
-				if ($resolve){
+				if ($resolve) {
 
-					if (preg_match("/[^\\.]+$/i",$row['filename'],$matches)){
+					if (preg_match("/[^\\.]+$/i", $row['filename'], $matches)) {
 
 						$ext = strtolower($matches[0]);
 						$guess = $path.'/images/thumb_'.$ext.'.jpg';
 						$file = @fopen($guess,"r");
-						if ($file){
+						if ($file) {
 							fclose($file);
 							echo '<a href="' . $path . '/displayimage.php?album=' . $albumid . '&pos=-' . $pos . '">' .
 								'<img src="' . $guess . '" title="' . $row['filename'] . '" alt="' . $row['filename'] . '" class="cm_thumbnail" style="width:' . $size . 'px; height:' . $size . 'px;"></a>';
@@ -357,7 +335,7 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 
 				$aspect = $width / $height;
 
-				if ($aspect >= 1){
+				if ($aspect >= 1) {
 
 					$width = $size;
 					$height = floor($size / $aspect);
@@ -381,8 +359,7 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 			}
 
 			if ($albumlink){
-
-				$albumresult = @mysql_query("SELECT * FROM " . $prefix . "albums WHERE aid = '" . $albumid . "'",$DB);
+				$albumresult = @mysqli_query($DB, "SELECT * FROM " . $prefix . "albums WHERE aid = '" . $albumid . "'");
 
 				if (!$albumresult) {
 					echo 'Error performing query: ' . mysql_error() . '<br>';
@@ -390,12 +367,9 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 					return;
 				}
 
-				while ( $albumname = mysql_fetch_array($albumresult) ) {
-
+				while ($albumname = mysqli_fetch_array($albumresult)) {
 					echo '<div class="cm_info"><a href="' . $path . '/thumbnails.php?album=' . $albumid . '">' . $albumname['title'] . '</a></div>';
-
 				}
-
 			}
 
 ?>
@@ -403,15 +377,11 @@ class serendipity_plugin_coppermine extends serendipity_plugin
 </div>
 
 <?php
-
 		}
-
 ?>
 
 <?php
-
-if (!empty($gallerypath)){
-
+if (!empty($gallerypath)) {
 ?>
 
 <div class="cm_gallery">
@@ -419,31 +389,24 @@ if (!empty($gallerypath)){
 </div>
 
 <?php
-
 }
-
 ?>
 
 </div>
 
 <?php
-
 		$this->finished($DB);
-
 	}
 
-	function finished(&$db){
-
+	function finished(&$db)
+    {
 		global $serendipity;
 
-		mysql_close($db);
+		mysqli_close($db);
 
-		$serendipity['dbConn'] = mysql_connect($serendipity['dbHost'], $serendipity['dbUser'], $serendipity['dbPass']);
-		mysql_select_db($serendipity['dbName']);
-
-
+		$serendipity['dbConn'] = mysqli_connect($serendipity['dbHost'], $serendipity['dbUser'], $serendipity['dbPass']);
+		mysqli_select_db($db, $serendipity['dbName']);
 	}
-
 
 }
 
