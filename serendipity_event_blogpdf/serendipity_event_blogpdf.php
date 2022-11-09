8fd1d636d61c9b959d601d6dc766a3a2d1b0531b
Ian <aristophian@googlemail.com>
2022-11-09 16:00:46 +0100

    blogpdf: Add another exclusion for entries footer, for filter_entries

--- serendipity_event_blogpdf/serendipity_event_blogpdf.php
+++ serendipity_event_blogpdf/serendipity_event_blogpdf.php
@@ -26,7 +26,7 @@ class serendipity_event_blogpdf extends serendipity_event
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Garvin Hicking, Olivier Plathey, Steven Wittens, Ian Styx');
         $propbag->add('license',       'GPL (Uses LGPL TCPDF');
-        $propbag->add('version',       '2.3.0');
+        $propbag->add('version',       '2.3.1');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
             'smarty'      => '3.1.0',
@@ -98,7 +98,7 @@ class serendipity_event_blogpdf extends serendipity_event
 
                 case 'entries_footer':
                     // don't do this in mode preview iframe, we use GET, since $serendipity['preview'] isn't available (yet?) and on staticpages or other plugin pages that are not categories!
-                    if (empty($serendipity['GET']['preview']) && isset($serendipity['view']) && in_array($serendipity['view'], ['start', 'entries', 'archives', 'entry', 'categories', 'plugin']) && (isset($eventData['id']) || (!isset($serendipity['viewtype']) || $serendipity['viewtype'] != '404_4')) && !isset($serendipity['is_staticpage']) && !isset($eventData['plugin_vars']['tag'])) {
+                    if (empty($serendipity['GET']['preview']) && isset($serendipity['view']) && in_array($serendipity['view'], ['start', 'entries', 'archives', 'entry', 'categories', 'plugin']) && false === ($serendipity['GET']['action'] === 'empty') && (isset($eventData['id']) || (!isset($serendipity['viewtype']) || $serendipity['viewtype'] != '404_4')) && !isset($serendipity['is_staticpage']) && !isset($eventData['plugin_vars']['tag'])) {
                         // entry views
                         if (isset($serendipity['GET']['id']) && is_numeric($serendipity['GET']['id'])) {
                             $links[] = '<a href="' . $serendipity['baseURL'] . ($serendipity['rewrite'] == 'none' ? $serendipity['indexFile'] . '?/' : '') . 'plugin/articlepdf_' . $serendipity['GET']['id'] . '">' . PLUGIN_EVENT_BLOGPDF_VIEW_ENTRY . '</a>';
