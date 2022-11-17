19e4d6cdd1041bf5c38a0cf1bdf24752c65f9553
Ian <aristophian@googlemail.com>
2022-11-17 14:42:52 +0100

    ckeplus: Improve dark_contents RT editor mode CSS for div block coloring

--- serendipity_event_ckeditor/serendipity_event_ckeditor.php
+++ serendipity_event_ckeditor/serendipity_event_ckeditor.php
@@ -201,7 +201,7 @@ class serendipity_event_ckeditor extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_CKEDITOR_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Rustam Abdullaev, Ian Styx');
-        $propbag->add('version',       '4.20.0.0'); // is CKEDITOR Series 4.20.0 - and appended plugin revision .0
+        $propbag->add('version',       '4.20.0.1'); // is CKEDITOR Series 4.20.0 - and appended plugin revision .1
         $propbag->add('copyright',     'GPL or LGPL License');
         $propbag->add('requirements',  array(
             'serendipity' => '2.6.2',
@@ -452,7 +452,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function updateConfig()
     {
-        #$this->temporaryDowngrade('4.20.0.0', '4.19.1.1'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.20.0.1', '4.20.0.0'); // was temporary used for the harmonization of plugin and lib versions
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
             $this->set_config('last_'.$match[0].'_version', $match[1]);
@@ -466,7 +466,7 @@ class serendipity_event_ckeditor extends serendipity_event
      */
     private function checkUpdate()
     {
-        #$this->temporaryDowngrade('4.20.0.0', '4.19.1.1'); // was temporary used for the harmonization of plugin and lib versions
+        #$this->temporaryDowngrade('4.20.0.1', '4.20.0.0'); // was temporary used for the harmonization of plugin and lib versions
         $doupdate = false;
         foreach(array_values($this->checkUpdateVersion) AS $package) {
             $match = explode(':', $package);
