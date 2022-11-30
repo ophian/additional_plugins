ophian/additional_plugins

commit 042f5b66e6575f2b329db01eac6c6b231008bc9d

podcast: Update getid3 lib to 1.9.22 and remove flash players

serendipity_event_podcast/serendipity_event_podcast.php
@@ -85,20 +85,11 @@ class serendipity_event_podcast extends serendipity_event
             'itunes_meta',
 
             /* players */
-            'extflow',
-            'extflow_player',
-
             'extquicktime',
             'extquicktime_player',
 
             'extwinmedia',
             'extwinmedia_player',
-
-            'extflash',
-            'extflash_player',
-
-            'extxspf',
-            'extxspf_player',
 
             'extaudio',
             'extaudio_player',
@@ -115,7 +106,7 @@ class serendipity_event_podcast extends serendipity_event
         ));
 
         $propbag->add('author', 'Grischa Brockhaus, Hannes Gassert, Garvin Hicking, Ian Styx');
-        $propbag->add('version', '1.47');
+        $propbag->add('version', '1.48');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
             'smarty'      => '3.0.0',
@@ -305,32 +296,11 @@ class serendipity_event_podcast extends serendipity_event
                 $propbag->add('default',        PLUGIN_PODCAST_WMEXT_DEFAULT);
                 break;
 
-            case 'extflash':
-                $propbag->add('type',           'string');
-                $propbag->add('name',           PLUGIN_PODCAST_MFEXT);
-                $propbag->add('description',    PLUGIN_PODCAST_MFEXT_DESC);
-                $propbag->add('default',        PLUGIN_PODCAST_MFEXT_DEFAULT);
-                break;
-
-            case 'extxspf':
-                $propbag->add('type',           'string');
-                $propbag->add('name',           PLUGIN_PODCAST_XSPFEXT);
-                $propbag->add('description',    PLUGIN_PODCAST_XSPFEXT_DESC);
-                $propbag->add('default',        PLUGIN_PODCAST_XSPFEXT_DEFAULT);
-                break;
-
             case 'extaudio':
                 $propbag->add('type',           'string');
                 $propbag->add('name',           PLUGIN_PODCAST_AUEXT);
                 $propbag->add('description',    PLUGIN_PODCAST_AUEXT_DESC);
                 $propbag->add('default',        PLUGIN_PODCAST_AUEXT_DEFAULT);
-                break;
-
-            case 'extflow_player':
-                $propbag->add('type',           'text');
-                $propbag->add('name',           PLUGIN_PODCAST_FLOWEXT_HTML);
-                $propbag->add('description',    '');
-                $propbag->add('default',        PLUGIN_PODCAST_FLOWPLAYER);
                 break;
 
             case 'exthtml5_audio_player':
@@ -359,20 +329,6 @@ class serendipity_event_podcast extends serendipity_event
                 $propbag->add('name',           PLUGIN_PODCAST_WMEXT_HTML);
                 $propbag->add('description',    '');
                 $propbag->add('default',        PLUGIN_PODCAST_WMPLAYER);
-                break;
-
-            case 'extflash_player':
-                $propbag->add('type',           'text');
-                $propbag->add('name',           PLUGIN_PODCAST_MFEXT_HTML);
-                $propbag->add('description',    '');
-                $propbag->add('default',        PLUGIN_PODCAST_FLASHPLAYER);
-                break;
-
-            case 'extxspf_player':
-                $propbag->add('type',           'text');
-                $propbag->add('name',           PLUGIN_PODCAST_XSPFEXT_HTML);
-                $propbag->add('description',    '');
-                $propbag->add('default',        PLUGIN_PODCAST_XSPFPLAYER);
                 break;
 
             case 'extaudio_player':
@@ -460,12 +416,6 @@ class serendipity_event_podcast extends serendipity_event
         }
 
         switch ($event) {
-            //////////////////////// Add Javascript for JW FLV Player ////////////////////////
-            case 'frontend_header':
-            case 'backend_header':
-                echo '<script type="text/javascript" src="' . $this->GetPluginHttpPath() . '/player/flowplayer/example/flowplayer-3.2.6.min.js"></script>' . "\n";
-                $this->log("Init\n--------------------------------------------------------------------------------------\n");
-                break;
 
             //////////////////////// RSS Entries ////////////////////////
             case 'frontend_display:rss-2.0:per_entry':
@@ -1222,14 +1172,11 @@ class serendipity_event_podcast extends serendipity_event
      */
     function InitializeSupportedFiletypes()
     {
-
         if (!isset($this->supportedFiletypes)) {
             $this->supportedFiletypes = array();
             $qtexts     = explode(',', $this->get_config('extquicktime', PLUGIN_PODCAST_QTEXT_DEFAULT));
             $wmexts     = explode(',', $this->get_config('extwinmedia', PLUGIN_PODCAST_WMEXT_DEFAULT));
-            $flexts     = explode(',', $this->get_config('extflash', PLUGIN_PODCAST_MFEXT_DEFAULT));
             $mp3exts    = explode(',', $this->get_config('extaudio', PLUGIN_PODCAST_AUEXT_DEFAULT));
-            $xspfexts   = explode(',', $this->get_config('extxspf', PLUGIN_PODCAST_XSPFEXT_DEFAULT));
             $flvexts    = explode(',', $this->get_config('extflow', PLUGIN_PODCAST_FLOWEXT_DEFAULT));
             $a5exts     = explode(',', $this->get_config('exthtml5_audio', PLUGIN_PODCAST_HTML5_AUDIO_DEFAULT));
             $v5exts     = explode(',', $this->get_config('exthtml5_video', PLUGIN_PODCAST_HTML5_VIDEO_DEFAULT));
@@ -1240,14 +1187,8 @@ class serendipity_event_podcast extends serendipity_event
             foreach($wmexts AS $ext){
                 $this->supportedFiletypes[trim($ext)] = 'w';
             }
-            foreach($flexts AS $ext){
-                $this->supportedFiletypes[trim($ext)] = 'f';
-            }
             foreach($mp3exts AS $ext){
                 $this->supportedFiletypes[trim($ext)] = 'm';
-            }
-            foreach($xspfexts AS $ext){
-                $this->supportedFiletypes[trim($ext)] = 'x';
             }
             foreach($flvexts AS $ext){
                 $this->supportedFiletypes[trim($ext)] = 'v';
@@ -1271,10 +1212,7 @@ class serendipity_event_podcast extends serendipity_event
             $this->supportedPlayers = array(
                 'q'  => $this->get_config('extquicktime_player', PLUGIN_PODCAST_QUICKTIMEPLAYER),
                 'w'  => $this->get_config('extwinmedia_player', PLUGIN_PODCAST_WMPLAYER),
-                'f'  => $this->get_config('extflash_player', PLUGIN_PODCAST_FLASHPLAYER),
-                'x'  => $this->get_config('extxspf_player', PLUGIN_PODCAST_XSPFPLAYER),
                 'm'  => $this->get_config('extaudio_player', PLUGIN_PODCAST_MP3PLAYER),
-                'v'  => $this->get_config('extflow_player', PLUGIN_PODCAST_FLOWPLAYER),
                 'a5' => $this->get_config('exthtml5_audio_player', PLUGIN_PODCAST_HTML5_AUDIOPLAYER),
                 'v5' => $this->get_config('exthtml5_video_player', PLUGIN_PODCAST_HTML5_VIDEOPLAYER),
             );

serendipity_event_podcast/UTF-8/lang_cs.inc.php
@@ -46,10 +46,7 @@
 @define('PLUGIN_PODCAST_QTEXT_DESC', 'Typy souborů, které je schopný přehrát Quick Time Player.');
 @define('PLUGIN_PODCAST_WMEXT', 'Windows Media Player přípony');
 @define('PLUGIN_PODCAST_WMEXT_DESC', 'Typy souborů, které je schopný přehrát Windows Media Player.');
-@define('PLUGIN_PODCAST_MFEXT', 'Flash přípony');
-@define('PLUGIN_PODCAST_MFEXT_DESC', 'Typy souborů, které je schopný přehrát Flash Player.');
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF flashplayer audio přípony');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Typy zvukových souborů, které je schopen přehrát XSFB flashplayer. Obvykle pouze MP3 a XSPF.');
+
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime miniplayer audio přípony');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Typy zvukových souborů, které je schopný přehrát Quick Time Miniplayer.');
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV player přípony');
@@ -83,12 +80,8 @@
 @define('PLUGIN_PODCAST_EXPERT_HINT', 'TIP: Pomocí HTML značek si můžete přizpůsobit LIBOVOLNÝ přehrávač, takže můžete zadat seznam různých variant přehrávače pro různé typy souborů! Pamatujte, že jak jednou uložíte nastavení pluginu, bude vždy použito statické značkování <strong>namísto</strong> toho, které plugin poskytuje pomocí souboru <strong>podcast_player.php</strong>. Pokud chcete resetovat nastavení na výchozí hodnoty, jednoduše vymažte veškerý obsah pole pro značkování pluginu a uložte nastavení.');
 @define('PLUGIN_PODCAST_QTEXT_HTML', 'Značkování přehrávače Quicktime');
 @define('PLUGIN_PODCAST_WMEXT_HTML', 'Značkování Windows Media Player');
-@define('PLUGIN_PODCAST_MFEXT_HTML', 'Značkování Flash player');
-@define('PLUGIN_PODCAST_XSPFEXT_HTML', 'Značkování XSPF');
 @define('PLUGIN_PODCAST_AUEXT_HTML', 'Značkování Quicktime.');
-@define('PLUGIN_PODCAST_FLOWEXT', 'Rozšíření Flowplayer');
-@define('PLUGIN_PODCAST_FLOWEXT_DESC', 'Rozšíření Flowplayer je schopné přehrávat formát FLV podporovaný Flashovými přehrávači. Více na www.flowplayer.org.');
-@define('PLUGIN_PODCAST_FLOWEXT_HTML', 'Značkování Flowplayer');
+
 @define('PLUGIN_PODCAST_HTML5_AUDIO', 'HTML5 audio rozšíření');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_DESC', 'Moderní prohlížeče nativně podporují HTML5 widgety.');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_HTML', 'Značkování HTML5 audio');

serendipity_event_podcast/UTF-8/lang_cz.inc.php
@@ -46,10 +46,7 @@
 @define('PLUGIN_PODCAST_QTEXT_DESC', 'Typy souborů, které je schopný přehrát Quick Time Player.');
 @define('PLUGIN_PODCAST_WMEXT', 'Windows Media Player přípony');
 @define('PLUGIN_PODCAST_WMEXT_DESC', 'Typy souborů, které je schopný přehrát Windows Media Player.');
-@define('PLUGIN_PODCAST_MFEXT', 'Flash přípony');
-@define('PLUGIN_PODCAST_MFEXT_DESC', 'Typy souborů, které je schopný přehrát Flash Player.');
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF flashplayer audio přípony');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Typy zvukových souborů, které je schopen přehrát XSFB flashplayer. Obvykle pouze MP3 a XSPF.');
+
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime miniplayer audio přípony');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Typy zvukových souborů, které je schopný přehrát Quick Time Miniplayer.');
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV player přípony');
@@ -83,12 +80,8 @@
 @define('PLUGIN_PODCAST_EXPERT_HINT', 'TIP: Pomocí HTML značek si můžete přizpůsobit LIBOVOLNÝ přehrávač, takže můžete zadat seznam různých variant přehrávače pro různé typy souborů! Pamatujte, že jak jednou uložíte nastavení pluginu, bude vždy použito statické značkování <strong>namísto</strong> toho, které plugin poskytuje pomocí souboru <strong>podcast_player.php</strong>. Pokud chcete resetovat nastavení na výchozí hodnoty, jednoduše vymažte veškerý obsah pole pro značkování pluginu a uložte nastavení.');
 @define('PLUGIN_PODCAST_QTEXT_HTML', 'Značkování přehrávače Quicktime');
 @define('PLUGIN_PODCAST_WMEXT_HTML', 'Značkování Windows Media Player');
-@define('PLUGIN_PODCAST_MFEXT_HTML', 'Značkování Flash player');
-@define('PLUGIN_PODCAST_XSPFEXT_HTML', 'Značkování XSPF');
 @define('PLUGIN_PODCAST_AUEXT_HTML', 'Značkování Quicktime.');
-@define('PLUGIN_PODCAST_FLOWEXT', 'Rozšíření Flowplayer');
-@define('PLUGIN_PODCAST_FLOWEXT_DESC', 'Rozšíření Flowplayer je schopné přehrávat formát FLV podporovaný Flashovými přehrávači. Více na www.flowplayer.org.');
-@define('PLUGIN_PODCAST_FLOWEXT_HTML', 'Značkování Flowplayer');
+
 @define('PLUGIN_PODCAST_HTML5_AUDIO', 'HTML5 audio rozšíření');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_DESC', 'Moderní prohlížeče nativně podporují HTML5 widgety.');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_HTML', 'Značkování HTML5 audio');

serendipity_event_podcast/UTF-8/lang_de.inc.php
@@ -45,10 +45,6 @@
 @define('PLUGIN_PODCAST_QTEXT_DESC', 'Erweiterungen, die mit dem Quicktime Player abgespielt werden sollen.');
 @define('PLUGIN_PODCAST_WMEXT', 'WindowsMediaPlayer Erweiterungen');
 @define('PLUGIN_PODCAST_WMEXT_DESC', 'Erweiterungen, die mit dem Windows Media Player abgespielt werden sollen.');
-@define('PLUGIN_PODCAST_MFEXT', 'Flash Erweiterungen');
-@define('PLUGIN_PODCAST_MFEXT_DESC', 'Erweiterungen, die mit dem Flash Player abgespielt werden sollen.');
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF Flashplayer Audio Erweiterungen');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Audio Erweiterungen, die mit dem XSPF Flashplayer abgespielt werden sollen. Dieser kann normaler Weise nur MP3 und XSPF Dateien abspielen.');
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime Miniplayer Audio Erweiterungen');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Audio Erweiterungen, die mit dem Quicktime Mini Player abgespielt werden sollen.');
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV Player Erweiterungen');

serendipity_event_podcast/lang_cs.inc.php
@@ -48,8 +48,7 @@
 @define('PLUGIN_PODCAST_WMEXT_DESC', 'Typy souborù, které je schopný pøehrát Windows Media Player.');
 @define('PLUGIN_PODCAST_MFEXT', 'Flash pøípony');
 @define('PLUGIN_PODCAST_MFEXT_DESC', 'Typy souborù, které je schopný pøehrát Flash Player.');
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF flashplayer audio pøípony');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Typy zvukových souborù, které je schopen pøehrát XSFB flashplayer. Obvykle pouze MP3 a XSPF.');
+
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime miniplayer audio pøípony');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Typy zvukových souborù, které je schopný pøehrát Quick Time Miniplayer.');
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV player pøípony');
@@ -84,11 +83,8 @@
 @define('PLUGIN_PODCAST_QTEXT_HTML', 'Znaèkování pøehrávaèe Quicktime');
 @define('PLUGIN_PODCAST_WMEXT_HTML', 'Znaèkování Windows Media Player');
 @define('PLUGIN_PODCAST_MFEXT_HTML', 'Znaèkování Flash player');
-@define('PLUGIN_PODCAST_XSPFEXT_HTML', 'Znaèkování XSPF');
 @define('PLUGIN_PODCAST_AUEXT_HTML', 'Znaèkování Quicktime.');
-@define('PLUGIN_PODCAST_FLOWEXT', 'Rozšíøení Flowplayer');
-@define('PLUGIN_PODCAST_FLOWEXT_DESC', 'Rozšíøení Flowplayer je schopné pøehrávat formát FLV podporovaný Flashovými pøehrávaèi. Více na www.flowplayer.org.');
-@define('PLUGIN_PODCAST_FLOWEXT_HTML', 'Znaèkování Flowplayer');
+
 @define('PLUGIN_PODCAST_HTML5_AUDIO', 'HTML5 audio rozšíøení');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_DESC', 'Moderní prohlížeèe nativnì podporují HTML5 widgety.');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_HTML', 'Znaèkování HTML5 audio');

serendipity_event_podcast/lang_cz.inc.php
@@ -48,8 +48,6 @@
 @define('PLUGIN_PODCAST_WMEXT_DESC', 'Typy souborù, které je schopný pøehrát Windows Media Player.');
 @define('PLUGIN_PODCAST_MFEXT', 'Flash pøípony');
 @define('PLUGIN_PODCAST_MFEXT_DESC', 'Typy souborù, které je schopný pøehrát Flash Player.');
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF flashplayer audio pøípony');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Typy zvukových souborù, které je schopen pøehrát XSFB flashplayer. Obvykle pouze MP3 a XSPF.');
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime miniplayer audio pøípony');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Typy zvukových souborù, které je schopný pøehrát Quick Time Miniplayer.');
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV player pøípony');
@@ -84,11 +82,8 @@
 @define('PLUGIN_PODCAST_QTEXT_HTML', 'Znaèkování pøehrávaèe Quicktime');
 @define('PLUGIN_PODCAST_WMEXT_HTML', 'Znaèkování Windows Media Player');
 @define('PLUGIN_PODCAST_MFEXT_HTML', 'Znaèkování Flash player');
-@define('PLUGIN_PODCAST_XSPFEXT_HTML', 'Znaèkování XSPF');
 @define('PLUGIN_PODCAST_AUEXT_HTML', 'Znaèkování Quicktime.');
-@define('PLUGIN_PODCAST_FLOWEXT', 'Roz¹íøení Flowplayer');
-@define('PLUGIN_PODCAST_FLOWEXT_DESC', 'Roz¹íøení Flowplayer je schopné pøehrávat formát FLV podporovaný Flashovými pøehrávaèi. Více na www.flowplayer.org.');
-@define('PLUGIN_PODCAST_FLOWEXT_HTML', 'Znaèkování Flowplayer');
+
 @define('PLUGIN_PODCAST_HTML5_AUDIO', 'HTML5 audio roz¹íøení');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_DESC', 'Moderní prohlí¾eèe nativnì podporují HTML5 widgety.');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_HTML', 'Znaèkování HTML5 audio');

serendipity_event_podcast/lang_de.inc.php
@@ -47,8 +47,7 @@
 @define('PLUGIN_PODCAST_WMEXT_DESC', 'Erweiterungen, die mit dem Windows Media Player abgespielt werden sollen.');
 @define('PLUGIN_PODCAST_MFEXT', 'Flash Erweiterungen');
 @define('PLUGIN_PODCAST_MFEXT_DESC', 'Erweiterungen, die mit dem Flash Player abgespielt werden sollen.');
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF Flashplayer Audio Erweiterungen');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Audio Erweiterungen, die mit dem XSPF Flashplayer abgespielt werden sollen. Dieser kann normaler Weise nur MP3 und XSPF Dateien abspielen.');
+
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime Miniplayer Audio Erweiterungen');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Audio Erweiterungen, die mit dem Quicktime Mini Player abgespielt werden sollen.');
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV Player Erweiterungen');

serendipity_event_podcast/lang_en.inc.php
@@ -59,20 +59,12 @@ simply delete all content in the markup textarea and save the plugin.');
 @define('PLUGIN_PODCAST_MFEXT_DESC', 'Extensions the Flash player is able to play.');
 @define('PLUGIN_PODCAST_MFEXT_HTML', 'Flash player markup');
 
-@define('PLUGIN_PODCAST_XSPFEXT', 'XSPF flashplayer audio extensions');
-@define('PLUGIN_PODCAST_XSPFEXT_DESC', 'Audio extensions the XSFP flashplayer is able to play. Normaly this is only MP3 and XSPF');
-@define('PLUGIN_PODCAST_XSPFEXT_HTML', 'XSPF markup');
-
 @define('PLUGIN_PODCAST_AUEXT', 'Quicktime miniplayer audio extensions');
 @define('PLUGIN_PODCAST_AUEXT_DESC', 'Audio extensions the quicktime miniplayer is able to play.');
 @define('PLUGIN_PODCAST_AUEXT_HTML', 'Quicktime markup.');
 
 @define('PLUGIN_PODCAST_FLVEXT', 'FLV player extensions');
 @define('PLUGIN_PODCAST_FLVEXT_DESC', 'Extensions the FLV player is able to play. FLV is a video format supported by Flash players. The benefit of this format is it\'s plattform independance. Normal video formats can be converted by free of cost tools (PC http://www.rivavx.com/index.php?id=483&L=0 und Mac http://www.versiontracker.com/dyn/moreinfo/macosx/15473).');
-
-@define('PLUGIN_PODCAST_FLOWEXT', 'Flowplayer extensions');
-@define('PLUGIN_PODCAST_FLOWEXT_DESC', 'Extensions the Flowplayer is able to play. FLV is a video format supported by Flash players. See more on www.flowplayer.org.');
-@define('PLUGIN_PODCAST_FLOWEXT_HTML', 'Flowplayer markup');
 
 @define('PLUGIN_PODCAST_HTML5_AUDIO', 'HTML5 audio extensions');
 @define('PLUGIN_PODCAST_HTML5_AUDIO_DESC', 'Modern browsers support HTML5 player widgets, native to the browser.');

commit 92f4220867e1caaa9266d1421e38fdad9f5547cb

podcast: Fix another regression of flash removal in 042f5b66e6575f2b329db01eac6c6b231008bc9d

serendipity_event_podcast/serendipity_event_podcast.php
@@ -106,7 +106,7 @@ class serendipity_event_podcast extends serendipity_event
         ));
 
         $propbag->add('author', 'Grischa Brockhaus, Hannes Gassert, Garvin Hicking, Ian Styx');
-        $propbag->add('version', '1.50');
+        $propbag->add('version', '1.51');
         $propbag->add('requirements',  array(
             'serendipity' => '2.0',
             'smarty'      => '3.0.0',
@@ -613,7 +613,7 @@ class serendipity_event_podcast extends serendipity_event
 
                 // First replace old style [podcast] syntax always, even without player replacement
                 if (is_array($eventData)) {
-                    if (preg_match($patterns['podcastLinkPattern'],$eventData['body'])) {
+                    if (preg_match($patterns['podcastLinkPattern'], $eventData['body'])) {
                         $eventData['body'] .= '<!-- old podcast style found -->';
                     }
                     $eventData['body'] = preg_replace(
@@ -624,7 +624,7 @@ class serendipity_event_podcast extends serendipity_event
                     $eventData['extended'] = preg_replace(
                         $patterns['podcastLinkPattern'],
                         '<a href="\4">\4</a>',
-                        $eventData['extended']);
+                        $eventData['extended'] ?? '');
                 }
 
                 // Now replace all links to mediafiles with the configured players:
@@ -637,7 +637,7 @@ class serendipity_event_podcast extends serendipity_event
                     $eventData['extended'] = preg_replace_callback(
                         $patterns['playerRewritePattern'],
                         array( $this, "playerRewriteCallBack"),
-                        $eventData['extended']);
+                        $eventData['extended'] ?? '');
                 }
 
                 // Check, if podcasts are added via the extended article attribute and add them to the article, if configured:
@@ -1182,9 +1182,6 @@ class serendipity_event_podcast extends serendipity_event
             }
             foreach($mp3exts AS $ext){
                 $this->supportedFiletypes[trim($ext)] = 'm';
-            }
-            foreach($flvexts AS $ext){
-                $this->supportedFiletypes[trim($ext)] = 'v';
             }
             foreach($a5exts AS $ext){
                 $this->supportedFiletypes[trim($ext)] = 'a5';