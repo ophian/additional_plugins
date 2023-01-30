ab8b521e76e8a7b3997aac8cef71dc627c5aaca8
Ian <aristophian@googlemail.com>
2023-01-30 19:45:10 +0100

    geo_json: Add to 2e729aa57b35c08d0044c67a4a62ec340d3b1e67

--- serendipity_event_geo_json/lang_en.inc.php
+++ serendipity_event_geo_json/lang_en.inc.php
@@ -1,5 +1,5 @@
 <?php
 
 @define('PLUGIN_EVENT_GEO_JSON_NAME', 'Geo-JSON');
-@define('PLUGIN_EVENT_GEO_JSON_DESC', 'Provides a JSON object with all geo data which can be processed by JavaScript.');
+@define('PLUGIN_EVENT_GEO_JSON_DESC', 'Provides a JSON object which contains all geographic data and can be processed with JavaScript');
 
