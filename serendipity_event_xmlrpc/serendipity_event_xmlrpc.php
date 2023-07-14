f5ad517cec4a425a3565804fecba5291d7bcfb4a
Ian <aristophian@googlemail.com>
2023-07-14 10:44:27 +0200

    xmlrpc: Add PHP 8.2: Sensitive Parameter value redaction support

--- serendipity_event_xmlrpc/serendipity_event_xmlrpc.php
+++ serendipity_event_xmlrpc/serendipity_event_xmlrpc.php
@@ -18,7 +18,7 @@ class serendipity_event_xmlrpc extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_XMLRPC_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Serendipity Team, Ian Styx');
-        $propbag->add('version',       '1.82');
+        $propbag->add('version',       '1.83');
         $propbag->add('requirements',  array(
             'serendipity' => '2.9',
             'smarty'      => '3.1',
