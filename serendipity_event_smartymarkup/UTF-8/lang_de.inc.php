7e134e6765d38bd9bc2c98eaada647fe84d7afb6
Ian <aristophian@googlemail.com>
2023-09-29 11:12:46 +0200

    smartymarkup: Remove from available Plugins to pluginheaven
    since Smarty [4.0.0-rc.0] from 2021-10-13:
       - $smarty->registerResource() no longer accepts an array of callback functions

--- serendipity_event_smartymarkup/UTF-8/lang_de.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-@define('PLUGIN_EVENT_SMARTYMARKUP_NAME', 'Textformatierung: Smarty Parsing');
-@define('PLUGIN_EVENT_SMARTYMARKUP_DESC', 'Parst Smarty-Anweisungen im Artikeltext. Dies ist nicht nutzbar in Kombination mit Syntax Code-Highlightern oder Multilingual tags in Textfeldern!');
-@define('PLUGIN_EVENT_SMARTYMARKUP_WARN', ' - Warnung: Kommentare sind öffentliche Eingaben. Benutze mit Vorsicht!');
-
