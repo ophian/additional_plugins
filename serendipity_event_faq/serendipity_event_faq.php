c0a0c88e2cc31faa0a99eeac3c595c153ed932b5
Ian <aristophian@googlemail.com>
2024-03-25 09:37:04 +0100

    faq: Fix for frontent consistent search result displays

--- serendipity_event_faq/serendipity_event_faq.php
+++ serendipity_event_faq/serendipity_event_faq.php
@@ -78,7 +78,7 @@ class serendipity_event_faq extends serendipity_event
         $propbag->add('name',         FAQ_NAME);
         $propbag->add('description',  FAQ_NAME_DESC);
         $propbag->add('author',       'Falk Doering, Ian Styx');
-        $propbag->add('version',      '1.59');
+        $propbag->add('version',      '1.60');
         $propbag->add('copyright',    'LGPL');
         $propbag->add('stackable',    false);
         $propbag->add('requirements', array(
