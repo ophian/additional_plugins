e6ea6c09656789d27f90d1fcd95a733c933676bc
Ian <1105372+ophian@users.noreply.github.com>
2025-04-13 12:22:35 +0200

    spamblock_surbl: Retire plugin - too old dependecies

--- serendipity_event_spamblock_surbl/lang_de.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-@define('PLUGIN_EVENT_SPAMBLOCKSURBL_TITLE', 'Spamschutz (SURBL)');
-@define('PLUGIN_EVENT_SPAMBLOCKSURBL_DESC', 'Kommentare abweisen deren URLs geblacklistet sind.');
-@define('PLUGIN_EVENT_SPAMBLOCK_ERROR_SURBL', 'Spamschutz: Ihre Nachricht enthält eine URL, die als SPAM geführt wird, daher wird Ihr Kommentar abgewiesen.');
-
