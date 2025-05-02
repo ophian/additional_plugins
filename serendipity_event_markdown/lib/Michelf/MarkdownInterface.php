13c51d2aeb87c445d50afb9e91187b85384fb702
Ian <1105372+ophian@users.noreply.github.com>
2025-05-02 15:14:06 +0200

    markdown: Update Markdown library update to v.2.0.0

--- serendipity_event_markdown/lib/Michelf/MarkdownInterface.php
+++ serendipity_event_markdown/lib/Michelf/MarkdownInterface.php
@@ -4,7 +4,7 @@
  *
  * @package   php-markdown
  * @author    Michel Fortin <michel.fortin@michelf.com>
- * @copyright 2004-2021 Michel Fortin <https://michelf.com/projects/php-markdown/>
+ * @copyright 2004-2022 Michel Fortin <https://michelf.com/projects/php-markdown/>
  * @copyright (Original Markdown) 2004-2006 John Gruber <https://daringfireball.net/projects/markdown/>
  */
 
@@ -14,25 +14,25 @@ namespace Michelf;
  * Markdown Parser Interface
  */
 interface MarkdownInterface {
-    /**
-     * Initialize the parser and return the result of its transform method.
-     * This will work fine for derived classes too.
-     *
-     * @api
-     *
-     * @param  string $text
-     * @return string
-     */
-    public static function defaultTransform($text);
+	/**
+	 * Initialize the parser and return the result of its transform method.
+	 * This will work fine for derived classes too.
+	 *
+	 * @api
+	 *
+	 * @param  string $text
+	 * @return string
+	 */
+	public static function defaultTransform(string $text): string;
 
-    /**
-     * Main function. Performs some preprocessing on the input text
-     * and pass it through the document gamut.
-     *
-     * @api
-     *
-     * @param  string $text
-     * @return string
-     */
-    public function transform($text);
+	/**
+	 * Main function. Performs some preprocessing on the input text
+	 * and pass it through the document gamut.
+	 *
+	 * @api
+	 *
+	 * @param  string $text
+	 * @return string
+	 */
+	public function transform(string $text): string;
 }
