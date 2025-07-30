bd0ccf4def83f5fcaf41dc39fe4e0ffba6f25c67
Ian <1105372+ophian@users.noreply.github.com>
2025-07-30 16:01:02 +0200

    textile: Fix PHP 8.4 deprecation error with lib3

--- serendipity_event_textile/serendipity_event_textile.php
+++ serendipity_event_textile/serendipity_event_textile.php
@@ -20,7 +20,7 @@ class serendipity_event_textile extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_TEXTILE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Serendipity Team, Lars Strojny, Ian Styx');
-        $propbag->add('version',       '2.0.0');
+        $propbag->add('version',       '2.0.1');
         $propbag->add('requirements',  array(
             'serendipity' => '5.0',
             'smarty'      => '4.1',
