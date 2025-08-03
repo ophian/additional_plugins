1c06c2368d965ea221252010570a5fe0eb99cb13
Ian <1105372+ophian@users.noreply.github.com>
2025-08-03 12:39:26 +0200

    markdown: Improve consistency of external link reference for frontend_comment hook

--- serendipity_event_markdown/serendipity_event_markdown.php
+++ serendipity_event_markdown/serendipity_event_markdown.php
@@ -27,7 +27,7 @@ class serendipity_event_markdown extends serendipity_event
             'smarty'      => '4.1',
             'php'         => '8.2'
         ));
-        $propbag->add('version',       '2.0.1');
+        $propbag->add('version',       '2.0.2');
         $propbag->add('cachable_events', array('frontend_display' => true));
         $propbag->add('event_hooks',   array(
             'backend_entryform' => true,
@@ -208,7 +208,7 @@ class serendipity_event_markdown extends serendipity_event
                 case 'frontend_comment':
                     // eventData allow_comments variable is only available in single page view commentform (not in eg. adduser sidebar)
                     if (serendipity_db_bool($this->get_config('COMMENT', 'true')) && !empty($eventData['allow_comments'])) {
-                        echo '                                <div class="serendipity_commentDirection serendipity_comment_markdown">' . PLUGIN_EVENT_MARKDOWN_TRANSFORM . '</div>';
+                        echo '                                <div class="serendipity_commentDirection serendipity_comment_markdown"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/><path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/><title>External link(s)</title></svg>' . PLUGIN_EVENT_MARKDOWN_TRANSFORM . '</div>';
                     }
                     break;
 
