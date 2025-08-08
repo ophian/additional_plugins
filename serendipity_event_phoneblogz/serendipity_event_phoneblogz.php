8cc84aa3883befb7d0fde25f52261fe8fdd23cfb
Ian <1105372+ophian@users.noreply.github.com>
2025-08-08 10:55:19 +0200

    phoneblogz: Fix-up for PHP 8.4  xml_set_* deprecation sets
    Function xml_set_object() is deprecated since 8.4, provide a proper method callable to xml_set_*_handler() functions in ...
    xml_set_element_handler(): Passing non-callable strings is deprecated since 8.4 in ...

--- serendipity_event_phoneblogz/serendipity_event_phoneblogz.php
+++ serendipity_event_phoneblogz/serendipity_event_phoneblogz.php
@@ -22,7 +22,7 @@ class serendipity_event_phoneblogz extends serendipity_event
         $propbag->add('description', PLUGIN_EVENT_PHONEBLOGZ_DESC);
         $propbag->add('stackable',   false);
         $propbag->add('author',      'Garvin Hicking, phoneblogz.com');
-        $propbag->add('version',     '1.0.0');
+        $propbag->add('version',     '1.1.0');
         $propbag->add('requirements',  array(
             'serendipity' => '5.0',
             'smarty'      => '4.1',
