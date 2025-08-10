69fed2ac743f102feea2eeecb90647453122b215
Ian <1105372+ophian@users.noreply.github.com>
2025-08-10 13:20:03 +0200

    spamblock_bee: Prep for PHP 8.5
    Non-canonical cast names (boolean), (integer), (double), and (binary) have been deprecated.

--- serendipity_event_spamblock_bee/json/JSON.php
+++ serendipity_event_spamblock_bee/json/JSON.php
@@ -660,8 +660,8 @@ class Services_JSON
                     // good about returning integers where appropriate:
                     // return (float)$str;
                     // Return float or int, as appropriate
-                    return ((float)$str == (integer)$str)
-                        ? (integer)$str
+                    return ((float)$str == (int)$str)
+                        ? (int)$str
                         : (float)$str;
 
                 } elseif (preg_match('/^("|\').*(\1)$/s', $str, $m) && $m[1] == $m[2]) {
--- serendipity_event_spamblock_bee/version.inc.php
+++ serendipity_event_spamblock_bee/version.inc.php
@@ -1,3 +1,3 @@
 <?php
 
-@define('PLUGIN_SPAMBLOCK_BEE_VERSION', '2.0.1');
+@define('PLUGIN_SPAMBLOCK_BEE_VERSION', '2.0.2');
