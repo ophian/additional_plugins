46b4a274a666ef65600db3a3c0edcc78036af2c9
Ian <1105372+ophian@users.noreply.github.com>
2025-08-10 15:51:28 +0200

    feed: Raise up for Styx 5.0

--- serendipity_event_feed/lang_de.inc.php
+++ serendipity_event_feed/lang_de.inc.php
@@ -5,9 +5,9 @@
 @define('PLUGIN_DASHBOARD_FEEDME_TITLE', 'Feedme');
 
 @define('PLUGIN_EVENT_FEED_SHOW_FEEDS', 'Zeige Feedbox im Dashboard');
-@define('PLUGIN_EVENT_FEED_FEEDS', 'W‰hle S9y Feed');
-@define('PLUGIN_EVENT_FEED_SHOW_FEEDNUM', 'W‰hle Anzahl von Feed Eintr‰gen');
-@define('PLUGIN_EVENT_FEED_SHOW_CONTENT', 'Zeige Feed Eintr‰ge on click?');
+@define('PLUGIN_EVENT_FEED_FEEDS', 'W√§hle S9y Feed');
+@define('PLUGIN_EVENT_FEED_SHOW_FEEDNUM', 'W√§hle Anzahl von Feed Eintr√§gen');
+@define('PLUGIN_EVENT_FEED_SHOW_CONTENT', 'Zeige Feed Eintr√§ge on click?');
 @define('PLUGIN_EVENT_FEED_SHOW_AUTHOR', 'Zeige Feed Autoren?');
 @define('PLUGIN_EVENT_FEED_SHOW_COMMENTS', 'Zeige Feed Kommentare?');
 
--- serendipity_event_feed/serendipity_event_feed.php
+++ serendipity_event_feed/serendipity_event_feed.php
@@ -1,6 +1,7 @@
 <?php
 
-// This line makes sure that plugins can only be called from the Serendipity Framework.
+declare(strict_types=1);
+
 if (IN_serendipity !== true) {
     die ("Don't hack!");
 }
@@ -20,11 +21,11 @@ class serendipity_event_feed extends serendipity_plugin
         $propbag->add('description',    PLUGIN_DASHBOARD_FEEDME_PLUGIN_DESC);
         $propbag->add('stackable',      false);
         $propbag->add('author',         'Ian Styx');
-        $propbag->add('version',        '1.05');
+        $propbag->add('version',        '2.0.0');
         $propbag->add('requirements',   array(
-            'serendipity' => '2.0.0',
-            'smarty'      => '3.1.0',
-            'php'         => '7.0.0'
+            'serendipity' => '5.0',
+            'smarty'      => '4.1',
+            'php'         => '8.2'
         ));
         $propbag->add('groups', array('BACKEND_ADMIN', 'BACKEND_DASHBOARD', 'BACKEND_FEATURES'));
         $propbag->add('event_hooks',    array(
