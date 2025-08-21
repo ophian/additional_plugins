a0a293af3e1e68b088a255e8aeb5db388c9cd368
Ian <1105372+ophian@users.noreply.github.com>
2025-08-21 09:48:14 +0200

    blogpdf: Remove a global where not in need

--- serendipity_event_blogpdf/serendipity_event_blogpdf.php
+++ serendipity_event_blogpdf/serendipity_event_blogpdf.php
@@ -22,14 +22,12 @@ class serendipity_event_blogpdf extends serendipity_event
 
     function introspect(&$propbag)
     {
-        global $serendipity;
-
         $propbag->add('name',          PLUGIN_EVENT_BLOGPDF_NAME);
         $propbag->add('description',   PLUGIN_EVENT_BLOGPDF_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Garvin Hicking, Olivier Plathey, Steven Wittens, Ian Styx');
         $propbag->add('license',       'GPL (Uses LGPL TCPDF');
-        $propbag->add('version',       '3.0.0');
+        $propbag->add('version',       '3.0.1');
         $propbag->add('requirements',  array(
             'serendipity' => '5.0',
             'smarty'      => '4.1',
