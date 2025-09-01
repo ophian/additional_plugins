8e0156613f4299f1ccda59cac19b6481aa73ac19
Ian <1105372+ophian@users.noreply.github.com>
2025-09-01 14:21:04 +0200

    wrapurl: Return and update from retired plugins since 8e869d635ce515e5d27de5f6a7cf968d6d696102
    Since this probably was a naming mistake. I can not remember why I would have decided to remove it if not... maybe this was because using iFrames and these were considered to be a really bad thing...

---
+++ serendipity_event_wrapurl/lang_de.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *  @version
+ *  @author Translator Name <yourmail@example.com>
+ *  DE-Revision: Revision of lang_de.inc.php
+ */
+
+@define('WRAPURL_TITLE', 'WrapURL');
+@define('WRAPURL_TITLE_BLAHBLAH', 'Umschließt eine weitere Seite in einem IFRAME, der im (body) Artikeltextbereich angezeigt werden soll.');
+@define('WRAPURL_HEADLINE', 'Überschrift');
+@define('WRAPURL_HEADLINE_BLAHBLAH', 'Zeigt eine Überschrift über der URL an, die wie jede andere Überschrift in Ihrem Blog dargestellt wird.');
+@define('WRAPURL_URL', 'Die URL');
+@define('WRAPURL_URL_BLAHBLAH', 'Die URL – entweder eine vollständige URL oder relativ zum Server-Dokumentenstammverzeichnis');
+@define('WRAPURL_HIGHT', 'Höhe in Pixeln');
+@define('WRAPURL_HIGHT_BLAHBLAH', 'Dies ist erforderlich, um das richtige CSS-Höhenattribut für den Iframe dieser einzelnen Seite festzulegen.');
+@define('WRAPURL_PERMALINK', 'Permalink');
+@define('WRAPURL_PAGETITLE', 'URL-Kurzname (Abwärtskompatibilität)');
+@define('WRAPURL_PAGETITLE_BLAHBLAH', 'Dies wird für die Benennung einzelner Unterseiten und CSS verwendet. Es ist ein einziges Wort erforderlich!');
+@define('WRAPURL_PERMALINK_BLAHBLAH', 'Definiert den Permalink für die URL. Benötigt den absoluten HTTP-Pfad und muss mit .htm oder .html enden!');
+@define('WRAPURL_URL_APPEND', 'GET-Variablen anhängen');
+@define('WRAPURL_URL_APPEND_BLAHBLAH', 'Sollen alle GET-Parameter an die Ziel-URL angehängt werden? Wenn diese Option auf „Ja“ gesetzt ist, werden alle zusätzlichen Parameter wie „&data1=X&data2=X“, die zum Aufrufen dieser WrapURL-Seite verwendet wurden, an die Ziel-URL innerhalb des Iframes angehängt. Wenn diese Option deaktiviert ist, wird nur die konfigurierte URL weitergegeben.');
+@define('WRAPURL_HIDE_SIDEBAR', 'Verstecke Seitenleisten?');
+
